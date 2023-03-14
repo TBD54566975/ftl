44800971d51223ed5f55db634d85d2fadbf77c9a
Dhanji R. Prasanna <d@wideplay.com>
2023-03-14 12:29:20 +1100

    HTTP Request params in context object

--- drive-kotlin/src/main/kotlin/xyz/block/ftl/Context.kt
+++ drive-kotlin/src/main/kotlin/xyz/block/ftl/Context.kt
@@ -3,13 +3,18 @@ package xyz.block.ftl
 import jakarta.servlet.http.HttpServletRequest
 import xyz.block.ftl.drive.verb.VerbDeck
 
-data class HttpSubContext(val headers: Map<String, String>)
+data class HttpSubContext(
+  val headers: Map<String, String>,
+  val params: Map<String, String>,
+  val uri: String)
 
 data class TraceSubContext(val verbsTransited: List<VerbDeck.VerbId>)
 
 class Context(
   val trace: TraceSubContext = TraceSubContext(listOf()),
-  val http: HttpSubContext?) {
+  private val http: HttpSubContext?) {
+
+  fun http(): HttpSubContext = http!!
 
   companion object {
     fun fromHttpRequest(verbId: VerbDeck.VerbId, request: HttpServletRequest): Context {
@@ -18,7 +23,14 @@ class Context(
         headers[name] = request.getHeader(name)
       }
 
-      return Context(TraceSubContext(listOf(verbId)), HttpSubContext(headers))
+      val params = mutableMapOf<String, String>()
+      request.parameterNames.asIterator().forEach { name ->
+        params[name] = request.getParameter(name)
+      }
+
+      return Context(
+        TraceSubContext(listOf(verbId)),
+        HttpSubContext(headers.toMap(), params.toMap(), request.requestURI))
     }
 
     fun fromLocal(verbId: VerbDeck.VerbId, propagator: Context) = Context(
