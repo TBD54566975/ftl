5678a8a51ffe3e9800bbbd2c6f6f0a50a0d1ed6f
Alec Thomas <alec@swapoff.org>
2023-03-28 13:12:48 +1100

    fix(jbr): use correct agent filename

--- bin/packages/jbr/post-unpack.sh
+++ bin/packages/jbr/post-unpack.sh
@@ -3,5 +3,5 @@ set -xeuo pipefail
 
 mkdir -p "$1/lib/hotswap"
 
-curl -fsSL -o "$1/lib/hotswap/hotswap-agent-1.4.1.jar" \
+curl -fsSL -o "$1/lib/hotswap/hotswap-agent.jar" \
   https://github.com/HotswapProjects/HotswapAgent/releases/download/RELEASE-1.4.1/hotswap-agent-1.4.1.jar
