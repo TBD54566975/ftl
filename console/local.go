aaacc4e8645c5522956018a8b49e91a387e5d751
Wes <wesbillman@users.noreply.github.com>
2023-04-28 08:51:34 -0700

    Swallow the Stdout for npm start up commands

--- console/local.go
+++ console/local.go
@@ -17,16 +17,16 @@ import (
 func Server(ctx context.Context) (http.Handler, error) {
 	logger := log.FromContext(ctx)
 	logger.Infof("Starting console dev server")
-	output := logger.WriterAt(log.Info)
 
-	err := exec.Command(ctx, "console", "npm", "install").Run()
+	cmd := exec.Command(ctx, "console", "npm", "install")
+	cmd.Stdout = nil
+	err := cmd.Run()
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	cmd := exec.Command(ctx, "console", "npm", "run", "dev")
-	cmd.Stdout = output
-	cmd.Stderr = output
+	cmd = exec.Command(ctx, "console", "npm", "run", "dev")
+	cmd.Stdout = nil
 	err = cmd.Start()
 	if err != nil {
 		return nil, errors.WithStack(err)
