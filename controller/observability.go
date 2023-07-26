f00901a6a636ebe00d661cbb408fdbea4db3677d
Wes <wesbillman@users.noreply.github.com>
2023-07-26 16:15:51 -0700

    Add more error handling to ftl calls (#133)

--- controller/observability.go
+++ controller/observability.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/TBD54566975/ftl/common/model"
 	"github.com/TBD54566975/ftl/controller/internal/dal"
+	"github.com/TBD54566975/ftl/internal/log"
 	ftlv1 "github.com/TBD54566975/ftl/protos/xyz/block/ftl/v1"
 	"github.com/TBD54566975/ftl/schema"
 )
@@ -29,10 +30,39 @@ func (s *Service) recordCall(ctx context.Context, call *Call) error {
 		sourceVerb = *call.callers[0]
 	}
 
-	var callError error
+	var responseError error
 	if call.response.GetError() != nil {
-		callError = errors.New(call.response.GetError().GetMessage())
+		responseError = errors.New(call.response.GetError().GetMessage())
 	}
+	err := s.dal.InsertCallEntry(ctx, &dal.CallEntry{
+		RequestKey:    call.requestKey,
+		RunnerKey:     call.runnerKey,
+		ControllerKey: call.controllerKey,
+		Duration:      time.Since(call.startTime),
+		SourceVerb:    sourceVerb,
+		DestVerb:      *call.destVerb,
+		Request:       call.request.GetBody(),
+		Response:      call.response.GetBody(),
+		Error:         responseError,
+	})
+
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+// recordCallError records a call that failed to be made.
+//
+// This is used when a call fails, but we still want to record the error.
+// Because of that, we only log failures within this function vs. returning another error.
+func (s *Service) recordCallError(ctx context.Context, call *Call, callError error) {
+	logger := log.FromContext(ctx)
+	sourceVerb := schema.VerbRef{}
+	if len(call.callers) > 0 {
+		sourceVerb = *call.callers[0]
+	}
+
 	err := s.dal.InsertCallEntry(ctx, &dal.CallEntry{
 		RequestKey:    call.requestKey,
 		RunnerKey:     call.runnerKey,
@@ -46,7 +76,6 @@ func (s *Service) recordCall(ctx context.Context, call *Call) error {
 	})
 
 	if err != nil {
-		return errors.WithStack(err)
+		logger.Errorf(err, "failed to record call error")
 	}
-	return nil
 }
