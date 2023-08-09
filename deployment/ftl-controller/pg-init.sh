24b4bc6a9424388fd08b3cac53dbdb5818787d3e
Alec Thomas <alec@swapoff.org>
2023-08-09 16:36:34 +1000

    feat: Kubernetes manifests and Kustomize config for ftl-controller
    So far this has been fairly straightforward to implement using
    Kustomize and a very handy Kubernetes operator for spinning up
    PostgreSQL.
    
    It uses the dbmate Docker container combined with a Kubernetes Job to
    perform the database migrations.
    
    And finally a deployment, service, HPA and ingress definition for
    starting ftl-controller and exposing it on the local machine's port
    8892.

--- deployment/ftl-controller/pg-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# https://www.kubegres.io/doc/getting-started.html
-kubectl apply -f https://raw.githubusercontent.com/reactive-tech/kubegres/v1.16/kubegres.yaml
-kubectl apply -f pg-cluster.yaml
-kubectl apply -f ftl-controller.yaml
-
--- examples/echo-kotlin/src/main/kotlin/ftl/echo/Echo.kt
+++ examples/echo-kotlin/src/main/kotlin/ftl/echo/Echo.kt
@@ -3,6 +3,8 @@ package ftl.echo
 import ftl.time.Time
 import ftl.time.TimeRequest
 import xyz.block.ftl.Context
+import xyz.block.ftl.Ingress
+import xyz.block.ftl.Method
 import xyz.block.ftl.Verb
 
 data class EchoRequest(val name: String)
@@ -11,6 +13,7 @@ data class EchoResponse(val message: String)
 
 class Echo {
   @Verb
+  @Ingress(Method.GET, "/echo")
   fun echo(context: Context, req: EchoRequest): EchoResponse {
     val response = context.call(Time::time, TimeRequest())
     return EchoResponse(message = "Hello, ${req.name}! The time is ${response.time}.")
