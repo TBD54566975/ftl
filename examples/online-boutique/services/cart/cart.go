148c9d793f7820bce85cd5c615fd3520008c9fca
Alec Thomas <alec@swapoff.org>
2023-08-23 16:36:07 +1000

    feat: implement boutique cart
    ```
    🐚 ~/…/online-boutique/services $ ftl call cart.addItem '{"userID": "alec", "item": {"productID": "foo", "quantity": 1}}'
    {}
    🐚 ~/…/online-boutique/services $ ftl call cart.getCart '{"UserID": "alec"}'
    {"UserID":"","Items":[{"ProductID":"foo","Quantity":1}]}
    🐚 ~/…/online-boutique/services $ ftl call cart.addItem '{"userID": "alec", "item": {"productID": "foo", "quantity": 1}}'
    {}
    🐚 ~/…/online-boutique/services $ ftl call cart.getCart '{"UserID": "alec"}'
    {"UserID":"","Items":[{"ProductID":"foo","Quantity":2}]}
    ```

--- examples/online-boutique/services/cart/cart.go
+++ examples/online-boutique/services/cart/cart.go
@@ -1,1 +1,52 @@
+//ftl:module cart
 package cart
+
+import (
+	"context"
+)
+
+var store = NewStore()
+
+type Item struct {
+	ProductID string
+	Quantity  int
+}
+
+type AddItemRequest struct {
+	UserID string
+	Item   Item
+}
+
+type AddItemResponse struct{}
+
+type Cart struct {
+	UserID string
+	Items  []Item
+}
+
+//ftl:verb
+func AddItem(ctx context.Context, req AddItemRequest) (AddItemResponse, error) {
+	store.Add(req.UserID, req.Item)
+	return AddItemResponse{}, nil
+}
+
+type GetCartRequest struct {
+	UserID string
+}
+
+//ftl:verb
+func GetCart(ctx context.Context, req GetCartRequest) (Cart, error) {
+	return Cart{Items: store.Get(req.UserID)}, nil
+}
+
+type EmptyCartRequest struct {
+	UserID string
+}
+
+type EmptyCartResponse struct{}
+
+//ftl:verb
+func EmptyCart(ctx context.Context, req EmptyCartRequest) (EmptyCartResponse, error) {
+	store.Empty(req.UserID)
+	return EmptyCartResponse{}, nil
+}
