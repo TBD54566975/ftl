72dd2ff7c22755d91d91c094a9af5302811dcc66
Wes <wesbillman@users.noreply.github.com>
2023-08-28 09:11:01 -0700

    Use better verbRef string format (#327)
    Fixes #319 
    <img width="473" alt="Screenshot 2023-08-28 at 9 06 18 AM"
    src="https://github.com/TBD54566975/ftl/assets/51647/4c607616-64ec-4a67-8aac-6b40f42ded06">
    <img width="484" alt="Screenshot 2023-08-28 at 9 06 27 AM"
    src="https://github.com/TBD54566975/ftl/assets/51647/3f18fc3d-c565-4ff5-a281-4d419d1cf5b7">

--- console/client/src/features/verbs/verb.utils.ts
+++ console/client/src/features/verbs/verb.utils.ts
@@ -1,4 +1,4 @@
-import { MetadataCalls, Verb } from '../../protos/xyz/block/ftl/v1/schema/schema_pb'
+import { MetadataCalls, Verb, VerbRef } from '../../protos/xyz/block/ftl/v1/schema/schema_pb'
 
 export function getCalls(verb?: Verb): MetadataCalls[] {
   return (
@@ -8,4 +8,8 @@ export function getCalls(verb?: Verb): MetadataCalls[] {
 
 export function buildVerbSchema(verbSchema: string, dataScemas: string[]): string {
   return dataScemas.join('\n\n') + '\n\n' + verbSchema
+}
+
+export function verbRefString(verb: VerbRef): string {
+  return `${verb.module}.${verb.name}`
 }
