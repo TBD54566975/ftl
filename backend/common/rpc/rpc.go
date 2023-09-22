75188ddaf604fa6a74db2b108217e92399da406f
Alec Thomas <alec@swapoff.org>
2023-09-22 16:08:29 +1000

    feat: add support for authentication plugins
    Authentication plugins accept the FTL endpoint URL as their argument and
    output one or more HTTP headers to be used for authentication. These
    headers will be added to every outbound request.
    
    There's an internal initial working proof of concept authentication
    plugin that uses [kooky](https://github.com/browserutils/kooky) to
    authenticate using an oauth2-proxy cookie. This seems to work well.

--- backend/common/rpc/rpc.go
+++ backend/common/rpc/rpc.go
@@ -13,48 +13,59 @@ import (
 	"github.com/jpillora/backoff"
 	"golang.org/x/net/http2"
 
+	"github.com/TBD54566975/ftl/authn"
 	"github.com/TBD54566975/ftl/backend/common/log"
 	ftlv1 "github.com/TBD54566975/ftl/protos/xyz/block/ftl/v1"
 )
 
-var (
-	dialer = &net.Dialer{
-		Timeout: time.Second * 10,
-	}
-	h2cClient = func() *http.Client {
-		var netTransport = &http2.Transport{
+// InitialiseClients HTTP clients used by the RPC system.
+//
+// "authenticators" are authenticator executables to use for each endpoint. The key is the URL of the endpoint, the
+// value is the path to the authenticator executable.
+func InitialiseClients(authenticators map[string]string) {
+	// We can't have a client-wide timeout because it also applies to
+	// streaming RPCs, timing them out.
+	h2cClient = &http.Client{
+		Transport: authn.Transport(&http2.Transport{
 			AllowHTTP: true,
-			DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+			DialTLSContext: func(ctx context.Context, network, addr string, _ *tls.Config) (net.Conn, error) {
 				conn, err := dialer.Dial(network, addr)
 				return conn, errors.WithStack(err)
 			},
-		}
-		return &http.Client{
-			// We can't have a client-wide timeout because it also applies to
-			// streaming RPCs, timing them out.
-			// Timeout:   time.Second * 10,
-			Transport: netTransport,
-		}
-	}()
-	tlsClient = func() *http.Client {
-		netTransport := &http2.Transport{
+		}, authenticators),
+	}
+	tlsClient = &http.Client{
+		Transport: authn.Transport(&http2.Transport{
 			DialTLSContext: func(ctx context.Context, network, addr string, config *tls.Config) (net.Conn, error) {
 				tlsDialer := tls.Dialer{Config: config, NetDialer: dialer}
 				conn, err := tlsDialer.DialContext(ctx, network, addr)
 				return conn, errors.WithStack(err)
 			},
-		}
-		return &http.Client{
-			// We can't have a client-wide timeout because it also applies to
-			// streaming RPCs, timing them out.
-			// Timeout:   time.Second * 10,
-			Transport: netTransport,
-		}
-	}()
+		}, authenticators),
+	}
+}
+
+var (
+	dialer = &net.Dialer{
+		Timeout: time.Second * 10,
+	}
+	h2cClient *http.Client
+	tlsClient *http.Client
 )
 
 type Pingable interface {
 	Ping(context.Context, *connect.Request[ftlv1.PingRequest]) (*connect.Response[ftlv1.PingResponse], error)
+}
+
+// GetHTTPClient returns a HTTP client usable for the given URL.
+func GetHTTPClient(url string) *http.Client {
+	if h2cClient == nil {
+		panic("rpc.InitialiseClients() must be called before GetHTTPClient()")
+	}
+	if strings.HasPrefix(url, "http://") {
+		return h2cClient
+	}
+	return tlsClient
 }
 
 // ClientFactory is a function that creates a new client and is typically one of
@@ -62,10 +73,7 @@ type Pingable interface {
 type ClientFactory[Client Pingable] func(httpClient connect.HTTPClient, baseURL string, opts ...connect.ClientOption) Client
 
 func Dial[Client Pingable](factory ClientFactory[Client], baseURL string, errorLevel log.Level, opts ...connect.ClientOption) Client {
-	client := tlsClient
-	if strings.HasPrefix(baseURL, "http://") {
-		client = h2cClient
-	}
+	client := GetHTTPClient(baseURL)
 	opts = append(opts, DefaultClientOptions(errorLevel)...)
 	return factory(client, baseURL, opts...)
 }
--- cmd/ftl/main.go
+++ cmd/ftl/main.go
@@ -9,10 +9,11 @@ import (
 	"syscall"
 
 	"github.com/alecthomas/kong"
+	"github.com/alecthomas/kong-toml"
 	"github.com/bufbuild/connect-go"
 
 	_ "github.com/TBD54566975/ftl/backend/common/automaxprocs" // Set GOMAXPROCS to match Linux container CPU quota.
-	log2 "github.com/TBD54566975/ftl/backend/common/log"
+	"github.com/TBD54566975/ftl/backend/common/log"
 	"github.com/TBD54566975/ftl/backend/common/rpc"
 	"github.com/TBD54566975/ftl/protos/xyz/block/ftl/v1/ftlv1connect"
 )
@@ -21,8 +22,11 @@ var version = "dev"
 
 type CLI struct {
 	Version   kong.VersionFlag `help:"Show version."`
-	LogConfig log2.Config      `embed:"" prefix:"log-" group:"Logging:"`
+	Config    kong.ConfigFlag  `help:"Load configuration from TOML file." placeholder:"FILE"`
+	LogConfig log.Config       `embed:"" prefix:"log-" group:"Logging:"`
 	Endpoint  *url.URL         `default:"http://127.0.0.1:8892" help:"FTL endpoint to bind/connect to." env:"FTL_ENDPOINT"`
+
+	Authenticators map[string]string `help:"Authenticators to use for FTL endpoints." mapsep:"," env:"FTL_AUTHENTICATORS" placeholder:"HOST=EXE,…"`
 
 	Status   statusCmd   `cmd:"" help:"Show FTL status."`
 	PS       psCmd       `cmd:"" help:"List deployments."`
@@ -40,6 +44,7 @@ var cli CLI
 func main() {
 	kctx := kong.Parse(&cli,
 		kong.Description(`FTL - Towards a 𝝺-calculus for large-scale systems`),
+		kong.Configuration(kongtoml.Loader, ".ftl.toml", "~/.ftl.toml"),
 		kong.AutoGroup(func(parent kong.Visitable, flag *kong.Flag) *kong.Group {
 			node, ok := parent.(*kong.Command)
 			if !ok {
@@ -54,13 +59,15 @@ func main() {
 		},
 	)
 
+	rpc.InitialiseClients(cli.Authenticators)
+
 	// Set the log level for child processes.
 	os.Setenv("LOG_LEVEL", cli.LogConfig.Level.String())
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	logger := log2.Configure(os.Stderr, cli.LogConfig)
-	ctx = log2.ContextWithLogger(ctx, logger)
+	logger := log.Configure(os.Stderr, cli.LogConfig)
+	ctx = log.ContextWithLogger(ctx, logger)
 
 	// Handle signals.
 	sigch := make(chan os.Signal, 1)
@@ -86,6 +93,6 @@ func main() {
 
 func makeDialer[Client rpc.Pingable](newClient func(connect.HTTPClient, string, ...connect.ClientOption) Client) func() (Client, error) {
 	return func() (Client, error) {
-		return rpc.Dial(newClient, cli.Endpoint.String(), log2.Error), nil
+		return rpc.Dial(newClient, cli.Endpoint.String(), log.Error), nil
 	}
 }
