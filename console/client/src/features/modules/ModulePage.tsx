ce1f5edac3b31bccd99cf36bc1a166a32b91ea50
Wes <wesbillman@users.noreply.github.com>
2023-09-29 15:33:31 -0700

    feat: add call list to module page (#428)
    ![Screenshot 2023-09-29 at 2 49
    44 PM](https://github.com/TBD54566975/ftl/assets/51647/6891f1cd-53de-43e2-82da-fcbbe1b2f07e)
    ![Screenshot 2023-09-29 at 2 49
    47 PM](https://github.com/TBD54566975/ftl/assets/51647/1db3087c-a84c-43b3-b684-0102dcc9809a)

--- console/client/src/features/modules/ModulePage.tsx
+++ console/client/src/features/modules/ModulePage.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { Card } from '../../components/Card'
 import { PageHeader } from '../../components/PageHeader'
-import { Module } from '../../protos/xyz/block/ftl/v1/console/console_pb'
+import { CallEvent, Module } from '../../protos/xyz/block/ftl/v1/console/console_pb'
 import { modulesContext } from '../../providers/modules-provider'
+import { SidePanelContext } from '../../providers/side-panel-provider'
+import { getCalls } from '../../services/console.service'
+import { formatTimestampShort } from '../../utils/date.utils'
+import { TimelineCallDetails } from '../timeline/details/TimelineCallDetails'
+import { verbRefString } from '../verbs/verb.utils'
 
 export const ModulePage = () => {
   const navigate = useNavigate()
   const { moduleName } = useParams()
+  const { openPanel, closePanel } = React.useContext(SidePanelContext)
   const modules = React.useContext(modulesContext)
   const [module, setModule] = React.useState<Module | undefined>()
+  const [calls, setCalls] = React.useState<CallEvent[] | undefined>()
+  const [selectedCall, setSelectedCall] = React.useState<CallEvent | undefined>()
 
   React.useEffect(() => {
     if (modules) {
@@ -19,25 +27,111 @@ export const ModulePage = () => {
     }
   }, [modules, moduleName])
 
+  React.useEffect(() => {
+    if (!module) return
+
+    const fetchCalls = async () => {
+      const calls = await getCalls(module.name)
+      setCalls(calls)
+    }
+    fetchCalls()
+  }, [module])
+
+  const handleCallClicked = (call: CallEvent) => {
+    if (selectedCall?.equals(call)) {
+      setSelectedCall(undefined)
+      closePanel()
+      return
+    }
+    setSelectedCall(call)
+    openPanel(<TimelineCallDetails timestamp={call.timeStamp!} call={call} />)
+  }
+
   return (
     <>
-      <PageHeader
-        icon={<Square3Stack3DIcon />}
-        title={module?.name || ''}
-        breadcrumbs={[{ label: 'Modules', link: '/modules' }]}
-      />
-      <div className='m-4'>
-        <div className='grid grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4'>
-          {module?.verbs.map((verb) => (
-            <Card
-              key={verb.verb?.name}
-              topBarColor='bg-green-500'
-              onClick={() => navigate(`/modules/${module.name}/verbs/${verb.verb?.name}`)}
-            >
-              {verb.verb?.name}
-              <p className='text-xs text-gray-400'>{verb.verb?.name}</p>
-            </Card>
-          ))}
+      <div className='flex-1 flex flex-col h-full'>
+        <div className='flex-1'>
+          <PageHeader
+            icon={<Square3Stack3DIcon />}
+            title={module?.name || ''}
+            breadcrumbs={[{ label: 'Modules', link: '/modules' }]}
+          />
+          <div className='flex-1 m-4'>
+            <div className='grid grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4'>
+              {module?.verbs.map((verb) => (
+                <Card
+                  key={verb.verb?.name}
+                  topBarColor='bg-green-500'
+                  onClick={() => navigate(`/modules/${module.name}/verbs/${verb.verb?.name}`)}
+                >
+                  {verb.verb?.name}
+                  <p className='text-xs text-gray-400'>{verb.verb?.name}</p>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className='flex-1 relative m-4'>
+          <div className='border border-gray-100 dark:border-slate-700 rounded h-full absolute inset-0'>
+            <table className={`w-full table-fixed text-gray-600 dark:text-gray-300`}>
+              <thead>
+                <tr className='flex text-xs'>
+                  <th className='p-1 text-left border-b w-40 border-gray-100 dark:border-slate-700 flex-none sticky top-0 z-10'>
+                    Date
+                  </th>
+                  <th className='p-1 text-left border-b w-40 border-gray-100 dark:border-slate-700 flex-none sticky top-0 z-10'>
+                    Source
+                  </th>
+                  <th className='p-1 text-left border-b w-40 border-gray-100 dark:border-slate-700 flex-none sticky top-0 z-10'>
+                    Destination
+                  </th>
+                  <th className='p-1 text-left border-b border-gray-100 dark:border-slate-700 flex-1 flex-grow sticky top-0 z-10'>
+                    Request
+                  </th>
+                  <th className='p-1 text-left border-b border-gray-100 dark:border-slate-700 flex-1 flex-grow sticky top-0 z-10'>
+                    Response
+                  </th>
+                  <th className='p-1 text-left border-b border-gray-100 dark:border-slate-700 flex-1 flex-grow sticky top-0 z-10'>
+                    Error
+                  </th>
+                </tr>
+              </thead>
+            </table>
+            <div className='overflow-y-auto h-[calc(100%-2rem)]'>
+              <table className={`w-full table-fixed text-gray-600 dark:text-gray-300`}>
+                <tbody className='text-xs'>
+                  {calls?.map((call, index) => (
+                    <tr
+                      key={`${index}-${call.timeStamp?.toDate().toUTCString()}`}
+                      className={`border-b border-gray-100 dark:border-slate-700 font-roboto-mono ${
+                        selectedCall?.equals(call) ? 'bg-indigo-50 dark:bg-slate-700' : ''
+                      } relative flex cursor-pointer hover:bg-indigo-50 dark:hover:bg-slate-700`}
+                      onClick={() => handleCallClicked(call)}
+                    >
+                      <td className='p-1 w-40 items-center flex-none text-gray-400 dark:text-gray-400'>
+                        {formatTimestampShort(call.timeStamp)}
+                      </td>
+                      <td className='p-1 w-40 flex-none text-indigo-500 dark:text-indigo-300'>
+                        {call.sourceVerbRef && verbRefString(call.sourceVerbRef)}
+                      </td>
+                      <td className='p-1 w-40 flex-none text-indigo-500 dark:text-indigo-300'>
+                        {call.destinationVerbRef && verbRefString(call.destinationVerbRef)}
+                      </td>
+                      <td className='p-1 flex-1 flex-grow truncate' title={call.request}>
+                        {call.request}
+                      </td>
+                      <td className='p-1 flex-1 flex-grow truncate' title={call.response}>
+                        {call.response}
+                      </td>
+                      <td className='p-1 flex-1 flex-grow truncate' title={call.error}>
+                        {call.error}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </>
