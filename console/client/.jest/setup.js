e40570efade016af4a700f05c8946cbbd09f2f03
Edward Irby <eirby@tbd.email>
2023-10-04 16:42:33 -0700

    test(clinet): mock ResizeObserver

--- console/client/.jest/setup.js
+++ console/client/.jest/setup.js
@@ -2,3 +2,9 @@ import * as React from 'react'
 import './expect-no-interdependencies.js'
 
 global.React = React
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
