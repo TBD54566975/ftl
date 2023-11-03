3839bbb38f292b240ba275a439ca4ed34b90ee32
Alec Thomas <aat@tbd.email>
2023-11-03 11:10:28 +1100

    refactor: add ZipRelative to zip files relative to the current package (#543)

---
+++ internal/zip_relative.go
@@ -0,0 +1,42 @@
+//go:build !release
+
+package internal
+
+import (
+	"archive/zip"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+// ZipRelativeToCaller creates a temporary zip file from a path relative to the caller.
+//
+// This function will leak a file descriptor and thus can only be used in development.
+func ZipRelativeToCaller(relativePath string) *zip.Reader {
+	_, file, _, _ := runtime.Caller(1)
+	dir := filepath.Join(filepath.Dir(file), relativePath)
+	w, err := os.CreateTemp("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(w.Name()) // This is okay because the zip.Reader will keep it open.
+	if err != nil {
+		panic(err)
+	}
+
+	err = ZipDir(dir, w.Name())
+	if err != nil {
+		panic(err)
+	}
+
+	info, err := w.Stat()
+	if err != nil {
+		panic(err)
+	}
+	_, _ = w.Seek(0, 0)
+	zr, err := zip.NewReader(w, info.Size())
+	if err != nil {
+		panic(err)
+	}
+	return zr
+}
