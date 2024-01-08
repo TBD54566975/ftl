75fca2ff47db79e0870998b0fc005704999b010c
worstell <worstell@squareup.com>
2024-01-08 16:50:29 -0500

    feat: add import schema command for translating into FTL types (#745)
    this change adds `ftl schema import <go/kotlin> <dir>`
    
    users can leverage this command to translate a directory containing
    arbitrary types into FTL-compliant types in a language of their
    choosing, Kotlin or Go
    
    The following is included in this PR:
    1. Add `ftl schema import...` commands, which
    2. Spin up container running a [local
    instance](https://github.com/jmorganca/ollama) of Meta's OSS AI model
    [Llama 2](https://ollama.ai/library/llama2) (if not already present) and
    3. Use [LangChain](https://github.com/tmc/langchaingo) to query the
    container programmatically
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- cmd/ftl/cmd_schema.go
+++ cmd/ftl/cmd_schema.go
@@ -4,4 +4,5 @@ type schemaCmd struct {
 	Get      getSchemaCmd      `default:"" cmd:"" help:"Retrieve the cluster FTL schema."`
 	Protobuf schemaProtobufCmd `cmd:"" help:"Generate protobuf schema mirroring the FTL schema structure."`
 	Generate schemaGenerateCmd `cmd:"" help:"Stream the schema from the cluster and generate files from the template."`
+	Import   schemaImportCmd   `cmd:"" help:"Import messages to the FTL schema."`
 }
---
+++ cmd/ftl/cmd_schema_import.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+
+	"github.com/TBD54566975/ftl/backend/common/exec"
+	"github.com/TBD54566975/ftl/backend/common/log"
+)
+
+type schemaImportCmd struct {
+	OllamaPort int             `help:"Port to use for ollama." default:"11434"`
+	Go         importGoCmd     `cmd:"" help:"Import types for an FTL Go module."`
+	Kotlin     importKotlinCmd `cmd:"" help:"Import types for an FTL Kotlin module."`
+}
+
+type importGoCmd struct {
+	Dir string `arg:"" required:"" help:"Directory to import from."`
+}
+
+type importKotlinCmd struct {
+	Dir string `arg:"" required:"" help:"Directory to import from."`
+}
+
+const ollamaContainerName = "ftl-ollama-1"
+const ollamaVolume = "ollama:/root/.ollama"
+const ollamaModel = "llama2"
+
+func (i *importKotlinCmd) getPrompt() (string, error) {
+	input, err := createInputString(i.Dir)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("I will provide you with an input string containing the contents of a file which must be translated into Kotlin. "+
+		"The resulting Kotlin file should translate every object from the input into its own Kotlin data class, written as `data class...`. "+
+		"The data class parameters should each be declared as `val`."+
+		"Everything in the provided input that is not a type or object declaration must be ignored."+
+		"No fields should be modified or added—the result must be an exact translation of the provided input, retaining case."+
+		"The result should reside in a package named `ftl`."+
+		"Your output should only include the resultant Kotlin data classes with no additional functions."+
+		"Please provide output that is only the resultant Kotlin file, with no additional text or explanation. The initial string to translate is this: %s",
+		input,
+	), nil
+}
+
+func (i *importGoCmd) getPrompt() (string, error) {
+	input, err := createInputString(i.Dir)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("I will provide you with an input string containing the contents of a file which must be translated into Go. "+
+		"The resulting Go file should represent every object as its own Go struct, written as `type...`. "+
+		"Everything in the provided input that is not a type or object declaration must be ignored."+
+		"No fields should be modified or added—the result must be an exact translation of the provided input, retaining case."+
+		"The result should reside in a package named `ftl`."+
+		"Your output should only include the resultant Go types with no additional functions."+
+		"Please provide output that is only the resultant Go file, with no additional text or explanation. The initial string to translate is this: %s",
+		input,
+	), nil
+}
+
+func (i *importGoCmd) Run(ctx context.Context, parent *schemaImportCmd) error {
+	err := parent.setup(ctx)
+	if err != nil {
+		return err
+	}
+
+	prompt, err := i.getPrompt()
+	if err != nil {
+		return err
+	}
+
+	err = query(ctx, prompt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *importKotlinCmd) Run(ctx context.Context, parent *schemaImportCmd) error {
+	err := parent.setup(ctx)
+	if err != nil {
+		return err
+	}
+
+	prompt, err := i.getPrompt()
+	if err != nil {
+		return err
+	}
+
+	err = query(ctx, prompt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func query(ctx context.Context, prompt string) error {
+	logger := log.FromContext(ctx)
+
+	logger.Infof("The import schema command relies on the %s AI chat model to translate schemas from other "+
+		"languages into FTL compliant objects in the specified language. Output may vary and results should be inspected "+
+		"for correctness. It is suggested that if the results are not satisfactory, you try again.\n\n", ollamaModel)
+
+	llm, err := ollama.New(ollama.WithModel("llama2"))
+	if err != nil {
+		logger.Errorf(err, "failed to call ollama")
+	}
+
+	completion, err := llm.Call(ctx, fmt.Sprintf("Human: %s \nAssistant:", prompt),
+		llms.WithTemperature(0.8),
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			fmt.Print(string(chunk))
+			return nil
+		}),
+	)
+	if err != nil {
+		logger.Errorf(err, "failed to call ollama")
+	}
+
+	_ = completion
+	return nil
+}
+
+func (s *schemaImportCmd) setup(ctx context.Context) error {
+	logger := log.FromContext(ctx)
+
+	nameFlag := fmt.Sprintf("name=^/%s$", ollamaContainerName)
+	output, err := exec.Capture(ctx, ".", "docker", "ps", "-a", "--filter", nameFlag, "--format", "{{.Names}}")
+	if err != nil {
+		logger.Errorf(err, "%s", output)
+		return err
+	}
+
+	port := fmt.Sprintf("%d", s.OllamaPort)
+
+	if len(output) == 0 {
+		logger.Infof("Creating docker container '%s' for ollama", ollamaContainerName)
+
+		// check if port s.OllamaPort is already in use
+		l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", s.OllamaPort))
+		if err == nil {
+			return fmt.Errorf("port %d is already in use", s.OllamaPort)
+		}
+		_ = l.Close()
+
+		err = exec.Command(ctx, logger.GetLevel(), "./", "docker", "run",
+			"-d", // run detached so we can follow with other commands
+			"-v", ollamaVolume,
+			"-p", fmt.Sprintf("%s:11434", port),
+			"--name", ollamaContainerName,
+			"ollama/ollama",
+		).Run()
+		if err != nil {
+			return err
+		}
+	} else {
+		// Start the existing container
+		_, err = exec.Capture(ctx, ".", "docker", "start", ollamaContainerName)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Initialize Ollama
+	err = exec.Command(ctx, logger.GetLevel(), "./", "docker", "exec",
+		ollamaContainerName,
+		"ollama",
+		"run", ollamaModel,
+	).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createInputString(dir string) (string, error) {
+	var result string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			fileContent, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			result += string(fileContent) + "\n"
+		}
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
