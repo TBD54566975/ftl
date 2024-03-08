759b2f3d93377c37ba70133767bc94a0c0bd66f3
Alec Thomas <aat@tbd.email>
2024-03-08 16:27:02 +1100

    chore: fix for local dev rebuilding ftl-runtime.jar on every run (#1049)
    This was occurring because frontend/local.go was calling `npm install`
    explicitly on every run, resulting in changes to the frontend that
    invalidated some of the dependencies of the JAR file.
    
    The fix is to use `bit` to build the frontend as well, resulting in its
    dependency cache being correct.

--- frontend/local.go
+++ frontend/local.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/TBD54566975/ftl/internal"
 	"github.com/TBD54566975/ftl/internal/cors"
 	"github.com/TBD54566975/ftl/internal/exec"
 	"github.com/TBD54566975/ftl/internal/log"
@@ -21,7 +22,7 @@ func Server(ctx context.Context, timestamp time.Time, publicURL *url.URL, allowO
 	logger := log.FromContext(ctx)
 	logger.Debugf("Building console...")
 
-	err := exec.Command(ctx, log.Debug, "frontend", "npm", "install").RunBuffered(ctx)
+	err := exec.Command(ctx, log.Debug, internal.GitRoot(""), "bit", "frontend/**/*").RunBuffered(ctx)
 	if err != nil {
 		return nil, err
 	}
