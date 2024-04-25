b34507d05f9d62d9f916b4b9999b95421d861847
Alec Thomas <aat@tbd.email>
2024-04-25 10:55:17 +1000

    refactor: move logic around filtering config for a module into the ConfigurationManager (#1330)

--- go-runtime/modulecontext/modulecontext.go
+++ go-runtime/modulecontext/modulecontext.go
@@ -2,8 +2,7 @@ package modulecontext
 
 import (
 	"context"
-
-	"github.com/alecthomas/types/optional"
+	"net/url"
 
 	ftlv1 "github.com/TBD54566975/ftl/backend/protos/xyz/block/ftl/v1"
 	cf "github.com/TBD54566975/ftl/common/configuration"
@@ -17,11 +16,11 @@ type ModuleContext struct {
 }
 
 func NewFromProto(ctx context.Context, moduleName string, response *ftlv1.ModuleContextResponse) (*ModuleContext, error) {
-	cm, err := newInMemoryConfigManager[cf.Configuration](ctx)
+	cm, err := newInMemoryConfigManager[cf.Configuration](ctx, response.Configs)
 	if err != nil {
 		return nil, err
 	}
-	sm, err := newInMemoryConfigManager[cf.Secrets](ctx)
+	sm, err := newInMemoryConfigManager[cf.Secrets](ctx, response.Secrets)
 	if err != nil {
 		return nil, err
 	}
@@ -31,12 +30,6 @@ func NewFromProto(ctx context.Context, moduleName string, response *ftlv1.Module
 		dbProvider:     NewDBProvider(),
 	}
 
-	if err := addConfigOrSecrets[cf.Configuration](ctx, *moduleCtx.configManager, response.Configs, moduleName); err != nil {
-		return nil, err
-	}
-	if err := addConfigOrSecrets[cf.Secrets](ctx, *moduleCtx.secretsManager, response.Secrets, moduleName); err != nil {
-		return nil, err
-	}
 	for _, entry := range response.Databases {
 		if err = moduleCtx.dbProvider.Add(entry.Name, DBType(entry.Type), entry.Dsn); err != nil {
 			return nil, err
@@ -45,23 +38,29 @@ func NewFromProto(ctx context.Context, moduleName string, response *ftlv1.Module
 	return moduleCtx, nil
 }
 
-func newInMemoryConfigManager[R cf.Role](ctx context.Context) (*cf.Manager[R], error) {
+func newInMemoryConfigManager[R cf.Role](ctx context.Context, config map[string][]byte) (*cf.Manager[R], error) {
 	provider := cf.NewInMemoryProvider[R]()
+	refs := map[cf.Ref]*url.URL{}
+	for name, data := range config {
+		ref := cf.Ref{Name: name}
+		u, err := provider.Store(ctx, ref, data)
+		if err != nil {
+			return nil, err
+		}
+		refs[ref] = u
+	}
 	resolver := cf.NewInMemoryResolver[R]()
+	for ref, u := range refs {
+		err := resolver.Set(ctx, ref, u)
+		if err != nil {
+			return nil, err
+		}
+	}
 	manager, err := cf.New(ctx, resolver, []cf.Provider[R]{provider})
 	if err != nil {
 		return nil, err
 	}
 	return manager, nil
-}
-
-func addConfigOrSecrets[R cf.Role](ctx context.Context, manager cf.Manager[R], valueMap map[string][]byte, moduleName string) error {
-	for name, data := range valueMap {
-		if err := manager.SetData(ctx, cf.Ref{Module: optional.Some(moduleName), Name: name}, data); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // ApplyToContext sets up the context so that configurations, secrets and DSNs can be retreived
