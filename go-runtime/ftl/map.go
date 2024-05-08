10dd581a373c5eddf273e7b2a0fbbe5217e4960c
Matt Toohey <mtoohey@block.xyz>
2024-05-08 11:23:25 +1000

    fix: remove once in ftl.Map to allow changing underlying values

--- go-runtime/ftl/map.go
+++ go-runtime/ftl/map.go
@@ -2,28 +2,22 @@ package ftl
 
 import (
 	"context"
-	"sync"
 )
 
 type MapHandle[T, U any] struct {
 	fn     func(context.Context, T) (U, error)
 	getter Handle[T]
-	out    U
-	once   *sync.Once
 }
 
-func (sh *MapHandle[T, U]) Get(ctx context.Context) U {
-	sh.once.Do(func() {
-		t, err := sh.fn(ctx, sh.getter.Get(ctx))
-		if err != nil {
-			panic(err)
-		}
-		sh.out = t
-	})
-	return sh.out
+func (mh *MapHandle[T, U]) Get(ctx context.Context) U {
+	t, err := mh.fn(ctx, mh.getter.Get(ctx))
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
 
 // Map an FTL resource type to a new type.
 func Map[T, U any](getter Handle[T], fn func(context.Context, T) (U, error)) MapHandle[T, U] {
-	return MapHandle[T, U]{fn: fn, getter: getter, once: &sync.Once{}}
+	return MapHandle[T, U]{fn: fn, getter: getter}
 }
