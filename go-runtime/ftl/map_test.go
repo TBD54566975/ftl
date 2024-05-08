acdb53a32568932faebf8e61822867dbb3dac1d6
Matt Toohey <mtoohey@block.xyz>
2024-05-08 12:14:35 +1000

    fix: remove once in ftl.Map to allow changing underlying values (#1435)
    fixes https://github.com/TBD54566975/ftl/issues/1390
    
    `ftl.Map` was just using `once` as an optimisation.
    But this unit test case was not being handled properly:
    ```
    ctx := ftltest.Context(
        ftltest.WithConfig(example, "hello"),
    )
    
    //Call a verb that uses ftl.Map with example config
    resp, err := Verb(ctx, req)
    
    ctx = ftltest.Context(
        ftltest.WithConfig(example, "world"),
    )
    
    //When this execution of the verb is done, it should execute again because the underlying value is different (hello -> world)
    resp, err = Verb(ctx, req)
    ```
    To solve this, now the mapping func is called each time.
    
    For use cases where modules want to map a `*sql.DB` to a `DAL`, it will
    create a new DAL instance each time

--- go-runtime/ftl/map_test.go
+++ go-runtime/ftl/map_test.go
@@ -12,21 +12,6 @@ type intHandle int
 
 func (s intHandle) Get(ctx context.Context) int { return int(s) }
 
-func TestMapBaseCase(t *testing.T) {
-	incrementer := 0
-
-	h := intHandle(123)
-	ctx := context.Background()
-	once := Map(h, func(ctx context.Context, n int) (string, error) {
-		incrementer++
-		return fmt.Sprintf("handle: %d", n), nil
-	})
-
-	assert.Equal(t, once.Get(ctx), "handle: 123")
-	assert.Equal(t, once.Get(ctx), "handle: 123")
-	assert.Equal(t, incrementer, 1)
-}
-
 func TestMapPanic(t *testing.T) {
 	ctx := context.Background()
 	n := intHandle(1)
