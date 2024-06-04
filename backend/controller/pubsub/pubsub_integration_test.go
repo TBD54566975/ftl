a7b42b447bf3638518deeacb4b8a7fd459906942
Matt Toohey <mtoohey@block.xyz>
2024-06-04 16:12:32 +1000

    add basic integration test

---
+++ backend/controller/pubsub/pubsub_integration_test.go
@@ -0,0 +1,41 @@
+//go:build integration
+
+package pubsub
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/TBD54566975/ftl/backend/controller/dal"
+	"github.com/TBD54566975/ftl/backend/schema"
+	in "github.com/TBD54566975/ftl/integration"
+)
+
+func TestPubSub(t *testing.T) {
+	in.Run(t, "",
+		in.CopyModule("publisher"),
+		in.CopyModule("subscriber"),
+		in.Deploy("publisher"),
+		in.Deploy("subscriber"),
+
+		// publish 3 events
+		in.Call("publisher", "publish", in.Obj{}, func(t testing.TB, resp in.Obj) {}),
+		in.Call("publisher", "publish", in.Obj{}, func(t testing.TB, resp in.Obj) {}),
+		in.Call("publisher", "publish", in.Obj{}, func(t testing.TB, resp in.Obj) {}),
+
+		// TODO: speed this up when we have proper pubsub implementation
+		in.Sleep(time.Second*4*3),
+
+		// check that there are 3 successful async calls
+		in.QueryRow("ftl",
+			fmt.Sprintf(`
+		SELECT COUNT(*)
+		FROM async_calls
+		WHERE
+			state = 'success'
+			AND origin = '%s'
+		`, dal.AsyncOriginPubSub{Subscription: schema.Ref{Module: "subscriber", Name: "test_subscription"}.ToRefKey()}.String()),
+			[]int64{3}),
+	)
+}
---
+++ backend/controller/pubsub/testdata/go/subscriber/subscriber.go
@@ -0,0 +1,18 @@
+package subscriber
+
+import (
+	"context"
+	"ftl/publisher"
+
+	"github.com/TBD54566975/ftl/go-runtime/ftl" // Import the FTL SDK.
+)
+
+var _ = ftl.RegisterSubscription(publisher.Test_topic, "test_subscription")
+
+//ftl:verb
+//ftl:subscribe test_subscription
+func Echo(ctx context.Context, req publisher.PubSubEvent) error {
+	logger := ftl.LoggerFromContext(ctx)
+	logger.Infof("Subscriber is processing %v", req.Time)
+	return nil
+}
