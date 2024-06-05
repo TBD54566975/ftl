41ba8a5ebd99280934971ee813a7be4972688ea8
Wes <wesbillman@users.noreply.github.com>
2024-06-05 15:10:16 -0700

    fix: forcefully shutdown ftl from extension

---
+++ extensions/vscode/src/client.ts
@@ -0,0 +1,138 @@
+import * as vscode from 'vscode'
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from 'vscode-languageclient/node'
+import { FTLStatus } from './status'
+
+export class FTLClient {
+  private clientName = 'ftl languge server'
+  private clientId = 'ftl'
+
+  private statusBarItem: vscode.StatusBarItem
+  private outputChannel: vscode.OutputChannel
+  private client: LanguageClient | undefined
+  private isClientStarting = false
+
+  constructor(statusBar: vscode.StatusBarItem, output: vscode.OutputChannel) {
+    this.statusBarItem = statusBar
+    this.outputChannel = output
+  }
+
+  public async start(ftlPath: string, cwd: string, flags: string[], context: vscode.ExtensionContext) {
+    if (this.client || this.isClientStarting) {
+      this.outputChannel.appendLine('FTL client already running or starting')
+      return
+    }
+    this.isClientStarting = true
+
+    this.outputChannel.appendLine('FTL extension activated')
+
+    const serverOptions: ServerOptions = {
+      run: {
+        command: `${ftlPath}`,
+        args: ['dev', ...flags],
+        options: { cwd: cwd }
+      },
+      debug: {
+        command: `${ftlPath}`,
+        args: ['dev', ...flags],
+        options: { cwd: cwd }
+      },
+    }
+
+    this.outputChannel.appendLine(`Running ${ftlPath} with flags: ${flags.join(' ')}`)
+    console.log(serverOptions.debug.args)
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [
+        { scheme: 'file', language: 'kotlin' },
+        { scheme: 'file', language: 'go' },
+      ],
+      outputChannel: this.outputChannel,
+    }
+
+    this.client = new LanguageClient(
+      this.clientId,
+      this.clientName,
+      serverOptions,
+      clientOptions
+    )
+
+    context.subscriptions.push(this.client)
+
+    this.outputChannel.appendLine('Starting lsp client')
+    try {
+      await this.client.start()
+      this.outputChannel.appendLine('Client started')
+      console.log(`${this.clientName} started`)
+      FTLStatus.started(this.statusBarItem)
+    } catch (error) {
+      console.error(`Error starting ${this.clientName}: ${error}`)
+      FTLStatus.error(this.statusBarItem, `Error starting ${this.clientName}: ${error}`)
+      this.outputChannel.appendLine(`Error starting ${this.clientName}: ${error}`)
+    }
+
+    this.isClientStarting = false
+  }
+
+  public async stop() {
+    if (!this.client && !this.isClientStarting) {
+      return
+    }
+
+    const timeout = 10000 // 10 seconds
+    if (this.isClientStarting) {
+      this.outputChannel.appendLine(`Waiting for client to complete startup before stopping`)
+      const startWaitTime = Date.now()
+      while (this.isClientStarting) {
+        await new Promise(resolve => setTimeout(resolve, 100))
+        if (Date.now() - startWaitTime > timeout) {
+          this.outputChannel.appendLine(`Timeout waiting for client to start`)
+          break
+        }
+      }
+    }
+
+    console.log('Stopping client')
+    const serverProcess = this.client!['_serverProcess']
+
+    try {
+      await this.client!.stop()
+      await this.client!.dispose()
+      this.client = undefined
+      console.log('Client stopped')
+    } catch (error) {
+      console.error('Error stopping client', error)
+    }
+
+    console.log('Stopping server process')
+    if (serverProcess && !serverProcess.killed) {
+      try {
+        process.kill(serverProcess.pid, 'SIGTERM')
+        // Wait a bit to see if the process terminates
+        await new Promise(resolve => setTimeout(resolve, 1000))
+
+        if (!serverProcess.killed) {
+          console.log('Server process did not terminate with SIGTERM, trying SIGKILL')
+          process.kill(serverProcess.pid, 'SIGKILL')
+          console.log('Server process terminated with SIGKILL')
+        }
+      } catch (error) {
+        console.log('SIGTERM failed, trying SIGKILL', error)
+        try {
+          // Forcefully terminate if SIGTERM fails
+          process.kill(serverProcess.pid, 'SIGKILL')
+          console.log('Server process terminiated with SIGKILL')
+        } catch (killError) {
+          console.log('Failed to kill server process', killError)
+        }
+      }
+    } else if (serverProcess && serverProcess.killed) {
+      console.log('Server process was already killed')
+    }
+
+    FTLStatus.stopped(this.statusBarItem)
+  }
+}
--- extensions/vscode/src/status.ts
+++ extensions/vscode/src/status.ts
@@ -1,25 +1,19 @@
-import * as vscode from "vscode"
+import * as vscode from 'vscode'
 
-export namespace FTLStatus {
-  export const starting = (statusBarItem: vscode.StatusBarItem) => {
+export const FTLStatus = {
+  starting: (statusBarItem: vscode.StatusBarItem) => {
     statusBarItem.text = `$(sync~spin) FTL`
-    statusBarItem.tooltip = "FTL is starting..."
-  }
-
-  export const started = (statusBarItem: vscode.StatusBarItem) => {
+    statusBarItem.tooltip = 'FTL is starting...'
+  },
+  started: (statusBarItem: vscode.StatusBarItem) => {
     statusBarItem.text = `$(zap) FTL`
-    statusBarItem.tooltip = "FTL is running."
-  }
-
-  export const stopped = (statusBarItem: vscode.StatusBarItem) => {
+    statusBarItem.tooltip = 'FTL is running.'
+  },
+  stopped: (statusBarItem: vscode.StatusBarItem) => {
     statusBarItem.text = `$(primitive-square) FTL`
-    statusBarItem.tooltip = "FTL is stopped."
-  }
-
-  export const error = (
-    statusBarItem: vscode.StatusBarItem,
-    message: string
-  ) => {
+    statusBarItem.tooltip = 'FTL is stopped.'
+  },
+  error: (statusBarItem: vscode.StatusBarItem, message: string) => {
     statusBarItem.text = `$(error) FTL`
     statusBarItem.tooltip = message
   }
