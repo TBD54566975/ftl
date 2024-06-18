88f7ddafa266882a420044b7df9819360c5a7ea5
Jon Johnson <113393155+jonathanj-square@users.noreply.github.com>
2024-06-18 11:58:32 -0700

    add test demonstrating dynamic update

--- internal/modulecontext/module_context_test.go
+++ internal/modulecontext/module_context_test.go
@@ -9,9 +9,30 @@ import (
 	"github.com/TBD54566975/ftl/internal/log"
 )
 
+type manualContextSupplier struct {
+	initialCtx ModuleContext
+	sink       ModuleContextSink
+}
+
 func TestGettingAndSettingFromContext(t *testing.T) {
 	ctx := log.ContextWithNewDefaultLogger(context.Background())
 	moduleCtx := NewBuilder("test").Build()
 	ctx = moduleCtx.MakeDynamic(ctx).ApplyToContext(ctx)
 	assert.Equal(t, moduleCtx, FromContext(ctx).CurrentContext(), "module context should be the same when read from context")
+}
+
+func TestDynamicContextUpdate(t *testing.T) {
+	ctx := log.ContextWithNewDefaultLogger(context.Background())
+	mc1 := NewBuilder("test").AddConfigs(map[string][]byte{"value": {0}}).Build()
+	mc2 := NewBuilder("test").AddConfigs(map[string][]byte{"value": {1}}).Build()
+	mcs := &manualContextSupplier{initialCtx: mc1}
+	dynamic, _ := NewDynamicContext(ctx, ModuleContextSupplier(mcs), "test")
+	assert.Equal(t, mc1, dynamic.CurrentContext())
+	mcs.sink(ctx, mc2)
+	assert.Equal(t, mc2, dynamic.CurrentContext())
+}
+
+func (mcs *manualContextSupplier) Subscribe(ctx context.Context, _ string, sink ModuleContextSink) {
+	sink(ctx, mcs.initialCtx)
+	mcs.sink = sink
 }
