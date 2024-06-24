e7dafeacf0f906ec1cd18bd145a5f55cb78a65c0
gak <gak@gak0.com>
2024-06-25 09:23:34 +1000

    wip: build schema command

--- rust-runtime/ftl/src/bin/ftl.rs
+++ rust-runtime/ftl/src/bin/ftl.rs
@@ -1,7 +1,12 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
+use prost::Message;
 use tracing::{error, info};
+
+use ftl::parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -12,13 +17,17 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    BuildSchema {
+        input: PathBuf,
+        output: PathBuf,
+    },
+    DumpModuleSchema {
+        file: PathBuf,
+    },
     CallVerb {
         module: String,
         verb: String,
         request: String,
-    },
-    DumpModule {
-        file: PathBuf,
     },
 }
 
@@ -28,6 +37,23 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     match cli.command {
+        Some(Commands::BuildSchema { input, output }) => {
+            info!("Building schema from {:?}", input);
+            let name = input.file_stem().unwrap().to_str().unwrap();
+            let content = std::fs::read_to_string(&input).unwrap();
+
+            let mut parser = parser::Parser::new();
+            let module = parser::ModuleIdent::new(name);
+            parser.add_module(&module, &content);
+
+            let proto = parser.generate_module_proto(&module);
+
+            let mut buf = Vec::new();
+            proto.encode(&mut buf).unwrap();
+
+            let mut file = File::create(&output).unwrap();
+            file.write_all(&buf).unwrap();
+        }
         Some(Commands::CallVerb {
             module,
             verb,
@@ -35,8 +61,9 @@ async fn main() {
         }) => {
             info!("Calling verb {} in module {}", verb, module);
             // ftl::verb_client::call_verb(module, verb, request).await;
+            todo!()
         }
-        Some(Commands::DumpModule { file }) => {
+        Some(Commands::DumpModuleSchema { file }) => {
             info!("Dumping {:?}", file);
             let reader = std::fs::File::open(&file).expect("unable to open file");
             let module = ftl::schema::binary_to_module(reader);
