7950ba51b833317cf9acfef818f5dc351b82252e
gak <gak@gak0.com>
2024-06-25 16:27:48 +1000

    lol

--- buildengine/build_rust.go
+++ buildengine/build_rust.go
@@ -11,12 +11,8 @@ import (
 func buildRustModule(ctx context.Context, _ *schema.Schema, module Module) error {
 	logger := log.FromContext(ctx)
 
-	if err := prepareFTLRoot(module); err != nil {
-		return fmt.Errorf("unable to prepare FTL root for %s: %w", module.Config.Module, err)
-	}
-
 	logger.Debugf("Using build command '%s'", module.Config.Build)
-	err := exec.Command(ctx, log.Debug, module.Config.Dir, "bash", "-c", module.Config.Build).RunBuffered(ctx)
+	err := exec.Command(ctx, log.Debug, module.Config.Dir+"/_ftl", "bash", "-c", module.Config.Build).RunBuffered(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to build module %q: %w", module.Config.Module, err)
 	}
