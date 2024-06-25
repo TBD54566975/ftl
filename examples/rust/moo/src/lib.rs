072794103a31c03d7a0e6039b5fa5f5a4718cd1a
gak <gak@gak0.com>
2024-06-25 16:47:52 +1000

    wip

--- examples/rust/moo/src/lib.rs
+++ examples/rust/moo/src/lib.rs
@@ -1,17 +1,20 @@
 use std::error::Error;
 use ftl::Context;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
     pub name: String,
     pub age: u32,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Response {
     pub message: String,
 }
 
 #[ftl::verb]
-pub async fn test_verb(ctx: &Context, request: Request) -> Result<Response, Box<dyn Error>> {
+pub async fn test_verb(ctx: Context, request: Request) -> Result<Response, Box<dyn Error>> {
     // let response = ctx.call(module::other_verb, request).await?;
 
     Ok(Response {
