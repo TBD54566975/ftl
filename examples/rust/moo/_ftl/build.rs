389ce1eda0ece5b90666ecde67e7039ba8ea886d
gak <gak@gak0.com>
2024-06-25 20:22:33 +1000

    wip

--- examples/rust/moo/_ftl/build.rs
+++ examples/rust/moo/_ftl/build.rs
@@ -2,13 +2,5 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let module_dir = Path::new("..");
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    let src = module_dir.join("src/lib.rs");
-    let call_immediate_path = Path::new(&out_dir).join("call_immediate.rs");
-    let schema_path = module_dir.join("_ftl").join("schema.pb");
-
-    // This does only one module/file
-    ftl::builder::build("moo", &src, &call_immediate_path, &schema_path);
+    ftl::builder::build("moo")
 }
--- rust-runtime/ftl/src/builder.rs
+++ rust-runtime/ftl/src/builder.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::path::Path;
 
 use clap::Parser;
@@ -17,32 +18,39 @@ struct Cli {
     config: Vec<String>,
 }
 
-/// The entrypoint for the generated runner.
+/// The entrypoint for the generated module.
 pub fn main(call_immediate_fn: CallImmediateFn) {
     let filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info,ftl=debug"));
     fmt::Subscriber::builder().with_env_filter(filter).init();
-    info!("Starting runner");
+    info!("Starting module...");
 
     let cli = Cli::parse();
 
     debug!(?cli);
 }
 
-pub fn build(module_name: &str, src_file: &Path, call_file: &Path, schema_file: &Path) {
+pub fn build(module_name: &str) {
+    let module_dir = Path::new("..");
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let src = module_dir.join("src/lib.rs");
+    let call_immediate_path = Path::new(&out_dir).join("call_immediate.rs");
+    let schema_path = module_dir
+        .join("_ftl")
+        .join("target")
+        .join("debug")
+        .join("schema.pb");
+
     let mut parser = parser::Parser::new();
     let module = parser::ModuleIdent::new(module_name);
-    let code = std::fs::read_to_string(src_file).unwrap();
+    let code = std::fs::read_to_string(&src).unwrap();
     parser.add_module(&module, &code);
-    assert!(
-        parser.modules_count() > 0,
-        "No modules found in {:?}",
-        src_file
-    );
-    parser.generate_call_immediate_file(call_file);
+    assert!(parser.modules_count() > 0, "No modules found in {:?}", src);
+    parser.generate_call_immediate_file(&call_immediate_path);
 
     let module = parser.generate_module_proto(&module);
     let mut encoded = Vec::new();
     module.encode(&mut encoded).unwrap();
-    std::fs::write(schema_file, encoded).unwrap();
+    std::fs::write(&schema_path, &encoded).unwrap();
 }
