2721f58680776f77acabdf0a3d11431e4d8e314d
Jon Johnson <113393155+jonathanj-square@users.noreply.github.com>
2024-06-25 16:40:07 -0700

    feat: Introduces support for other node types (#1875)
    adds: configs, enums, fsms, secrets, cron, pubsub, and databases

--- extensions/vscode/src/commands/node-new.ts
+++ extensions/vscode/src/commands/node-new.ts
@@ -23,7 +23,7 @@ export const nodeNewCommand = async (item: FtlTreeItem) => {
 
   //TODO: Add all the types here...
   // Also would be cool to have an httpingress type which would populate a valid //ftl:ingress ....
-  const nodeType = await vscode.window.showQuickPick(['verb', 'enum'], {
+  const nodeType = await vscode.window.showQuickPick(['verb', 'enum', 'pubsub', 'pubsub:subscription', 'fsm', 'database', 'config:string', 'config:struct', 'secret', 'cron'], {
     title: 'Which type of node would you like to add',
     placeHolder: 'Choose a node type',
     canPickMany: false,
@@ -59,7 +59,42 @@ const snippetForNodeType = (nodeType: string): string => {
   //TODO: fill out for all node types.
   switch (nodeType) {
     case 'verb':
-      return `type SampleRequest struct {
+      return verbSnippet
+    case 'enum':
+      return enumSnippet
+
+    case 'pubsub':
+      return publisherSnippet + '\n\n' + subscriberSnippet
+
+    case 'pubsub:subscription':
+      return subscriberSnippet
+
+    case 'fsm':
+      return fsmSnippet
+
+    case 'database':
+      return `var sampleDatabase = ftl.PostgresDatabase("sample_db")`
+
+    case 'config:string':
+      return `var sampleConfig = ftl.Config[string]("sample_config")`
+
+    case 'config:struct':
+      return configStructSnippet
+
+    case 'secret':
+      return `var sampleSecret = ftl.Secret[string]("sample_secret")`
+
+    case 'cron':
+      return cronSnippet
+
+    // Add more cases here for other node types
+  }
+
+  return ''
+  // vscode.window.showInformationMessage(`Adding a new ${nodeType} node to ${document.uri.toString()}`)
+}
+
+const verbSnippet = `type SampleRequest struct {
 	Name string
 }
 
@@ -71,12 +106,77 @@ type SampleResponse struct {
 func Sample(ctx context.Context, req SampleRequest) (SampleResponse, error) {
 	return SampleResponse{Message: "Hello, world!"}, nil
 }`
-    case 'enum':
-      return `put enum stuff here (maybe type and value enums?)`
 
-    // Add more cases here for other node types
-  }
+const enumSnippet = `//ftl:enum
+type SampleEnum string
+const (
+	FirstValue SampleEnum = "first"
+	SecondValue SampleEnum = "second"
+)`
 
-  return ''
-  // vscode.window.showInformationMessage(`Adding a new ${nodeType} node to ${document.uri.toString()}`)
+const publisherSnippet = `//ftl:export
+var sampleTopic = ftl.Topic[SamplePubSubEvent]("sample_topic")
+
+type SamplePubSubEvent struct {
+	Message string
+}`
+
+const subscriberSnippet = `var _ = ftl.Subscription(sampleTopic, "sample_subscription")
+
+//ftl:verb
+//ftl:subscribe sample_subscription
+func SampleSubscriber(ctx context.Context, event SamplePubSubEvent) error {
+	return nil
+}`
+
+const fsmSnippet = `type SampleFSMMessage struct {
+	Instance string
+	Message string
 }
+
+var sampleFsm = ftl.FSM("sample_fsm",
+	ftl.Start(SampleFSMState0),
+	ftl.Transition(SampleFSMState0, SampleFSMState1),
+	ftl.Transition(SampleFSMState1, SampleFSMState2),
+)
+
+//ftl:verb
+func SampleFSMState0(ctx context.Context, in SampleFSMMessage) error {
+	logger := ftl.LoggerFromContext(ctx)
+	logger.Infof("message %q entering state 0", in.Message)
+	return nil
+}
+
+//ftl:verb
+func SampleFSMState1(ctx context.Context, in SampleFSMMessage) error {
+	logger := ftl.LoggerFromContext(ctx)
+	logger.Infof("message %q entering state 1", in.Message)
+	return nil
+}
+
+//ftl:verb
+func SampleFSMState2(ctx context.Context, in SampleFSMMessage) error {
+	logger := ftl.LoggerFromContext(ctx)
+	logger.Infof("message %q entering state 2", in.Message)
+	return nil
+}
+
+//ftl:verb
+func SendSampleFSMMessage(ctx context.Context, in SampleFSMMessage) error {
+	return sampleFsm.Send(ctx, in.Instance, in)
+}
+`
+
+const configStructSnippet = `type SampleConfig struct {
+	Field string
+}
+
+var sampleConfigValue = ftl.Config[SampleConfig]("sample_config")`
+
+const cronSnippet = `// This cron job will run every 5 minutes
+//ftl:cron * /5 * * * * *
+func SampleCron(ctx context.Context) error {
+	logger := ftl.LoggerFromContext(ctx)
+	logger.Infof("sample cron job triggered")
+	return nil
+}`
