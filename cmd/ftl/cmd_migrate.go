2261d333aeb7fe8eee38f7054a604b6a10a22adb
gak <gak@gak0.com>
2024-07-10 15:11:15 +1000

    chore: lint

--- cmd/ftl/cmd_migrate.go
+++ cmd/ftl/cmd_migrate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/TBD54566975/ftl/internal/log"
 
 	"github.com/TBD54566975/ftl/backend/controller/sql"
@@ -14,5 +15,6 @@ type migrateCmd struct {
 func (c *migrateCmd) Run(ctx context.Context) error {
 	logger := log.FromContext(ctx)
 	logger.Infof("Migrating database")
-	return sql.Migrate(ctx, c.DSN)
+	err := sql.Migrate(ctx, c.DSN)
+	return fmt.Errorf("failed to migrate database: %w", err)
 }
