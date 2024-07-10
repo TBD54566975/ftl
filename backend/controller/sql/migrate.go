7469ad453dad447449ef847b56be146241254601
gak <gak@gak0.com>
2024-07-10 16:58:42 +1000

    feat: ftl migrate command (#2033)
    Fixes #1839
    
    - Migration files are embedded in the binary, so the binary version
    matches the schema.
    - Uses `DATABASE_URL` (same as dbmate) or `--dsn` to specify the
    connection.
    
    <img width="909" alt="image"
    src="https://github.com/TBD54566975/ftl/assets/31338/81b6fc6c-ede4-42c4-a5d5-1ed2d7b02342">

--- backend/controller/sql/migrate.go
+++ backend/controller/sql/migrate.go
@@ -18,7 +18,7 @@ import (
 var migrationSchema embed.FS
 
 // Migrate the database.
-func Migrate(ctx context.Context, dsn string) error {
+func Migrate(ctx context.Context, dsn string, logLevel log.Level) error {
 	u, err := url.Parse(dsn)
 	if err != nil {
 		return fmt.Errorf("invalid DSN: %w", err)
@@ -31,7 +31,7 @@ func Migrate(ctx context.Context, dsn string) error {
 
 	db := dbmate.New(u)
 	db.FS = migrationSchema
-	db.Log = log.FromContext(ctx).Scope("migrate").WriterAt(log.Debug)
+	db.Log = log.FromContext(ctx).Scope("migrate").WriterAt(logLevel)
 	db.MigrationsDir = []string{"schema"}
 	err = db.CreateAndMigrate()
 	if err != nil {
