ff9a968d2fe830989adc1bea23776dc5322d828c
Wes <wesbillman@users.noreply.github.com>
2024-07-10 17:13:22 -0700

    fix: sync stub references for each build group (#2039)
    This PR modifies the build engine to generate stubs for all known
    schemas at build time. After a build group is built, we then sync the
    stub references to help users get auto completions and imports in their
    IDEs or text editors.
    
    It essentially changes the way we gather schemas for the module(s) we're
    building.
    
    In `go` syncing the stub references means that we update the `go.work`
    file for each built module to include ALL shared module stubs. This
    helps when a module is adding a shared module as a dependency.

--- buildengine/stubs.go
+++ buildengine/stubs.go
@@ -21,6 +21,13 @@ func CleanStubs(ctx context.Context, projectRoot string) error {
 	return cleanGoStubs(ctx, projectRoot)
 }
 
+// SyncStubReferences syncs the references in the generated stubs.
+//
+// For Go, this means updating all the go.work files to include all known modules in the shared stubbed modules directory.
+func SyncStubReferences(ctx context.Context, projectRoot string, moduleNames []string, moduleConfigs []moduleconfig.ModuleConfig) error {
+	return syncGoStubReferences(ctx, projectRoot, moduleNames, moduleConfigs)
+}
+
 func generateGoStubs(ctx context.Context, projectRoot string, modules []*schema.Module, moduleConfigs []moduleconfig.ModuleConfig) error {
 	sch := &schema.Schema{Modules: modules}
 	err := compile.GenerateStubsForModules(ctx, projectRoot, moduleConfigs, sch)
@@ -34,6 +41,14 @@ func cleanGoStubs(ctx context.Context, projectRoot string) error {
 	err := compile.CleanStubs(ctx, projectRoot)
 	if err != nil {
 		return fmt.Errorf("failed to clean go stubs: %w", err)
+	}
+	return nil
+}
+
+func syncGoStubReferences(ctx context.Context, projectRoot string, moduleNames []string, moduleConfigs []moduleconfig.ModuleConfig) error {
+	err := compile.SyncGeneratedStubReferences(ctx, projectRoot, moduleNames, moduleConfigs)
+	if err != nil {
+		fmt.Printf("failed to sync go stub references: %v\n", err)
 	}
 	return nil
 }
