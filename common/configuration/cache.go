89bcfc1c3af4eb6eea6654563abc5a276eaecd3f
Matt Toohey <mtoohey@block.xyz>
2024-07-26 14:54:11 +1000

    fix: secrets cache no longer busy loops when no providers to sync

--- common/configuration/cache.go
+++ common/configuration/cache.go
@@ -145,6 +145,7 @@ func (c *cache[R]) sync(ctx context.Context) {
 
 		// Can not calculate next sync date for each provider as sync intervals can change (eg when follower becomes leader)
 		case <-time.After(time.Until(next)):
+			next = time.Now().Add(time.Second)
 			wg := &sync.WaitGroup{}
 
 			providersToSync := []*cacheProvider[R]{}
@@ -172,7 +173,6 @@ func (c *cache[R]) sync(ctx context.Context) {
 				}(cp)
 			}
 			wg.Wait()
-			next = time.Now().Add(time.Second)
 		}
 	}
 }
