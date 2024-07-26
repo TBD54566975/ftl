90d0531db88856740173e85e528f9c5685166266
Jon Johnson <113393155+jonathanj-square@users.noreply.github.com>
2024-07-25 17:48:50 -0700

    fixing test failure caused by missed instrumentation initialization

--- backend/controller/dal/fsm_test.go
+++ backend/controller/dal/fsm_test.go
@@ -2,6 +2,7 @@ package dal
 
 import (
 	"context"
+	"github.com/TBD54566975/ftl/backend/controller/observability"
 	"testing"
 	"time"
 
@@ -15,6 +16,8 @@ import (
 )
 
 func TestSendFSMEvent(t *testing.T) {
+	_ = observability.InitControllerObservability()
+
 	ctx := log.ContextWithNewDefaultLogger(context.Background())
 	conn := sqltest.OpenForTesting(ctx, t)
 	dal, err := New(ctx, conn)
