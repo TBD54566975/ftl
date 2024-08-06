8fcbd33410fd3cc41d23e4c54a302d0b40dd8f18
Denise Li <dli@squareup.com>
2024-08-06 15:39:32 -0400

    feat: bucketize latency attribute (#2269)
    This PR bucketizes the latency _attribute_ (NOT the latency _metric_!
    That would be very bad!)
    
    For better filtering capabilities in datadog, we need to bucket our
    high-cardinality numeric attributes. This uses log base8 because:
    
    1. That seems to give the most useful results for current production
    numbers we're seeing in DD - good functional granularity for analysis
    purposes without being so granular that it's annoying to sort through
    the buckets. Current values range from 62 to 20279, concentrated in the
    100-1000 range.
    2. Base 8 bucket labels are all powers of 2, so it's relatively easy for
    our brains to skim
    
    Sample output:
    ```
    -> ftl.async_call.time_since_scheduled_at_ms.bucket: Str([8,64))
    ```
    
    Issue: https://github.com/TBD54566975/ftl/issues/2194

--- backend/controller/observability/observability.go
+++ backend/controller/observability/observability.go
@@ -3,6 +3,7 @@ package observability
 import (
 	"errors"
 	"fmt"
+	"math"
 	"time"
 
 	"go.opentelemetry.io/otel/metric"
@@ -46,4 +47,21 @@ func handleInt64UpDownCounterError(counter string, err error, errs error) (metri
 
 func timeSinceMS(start time.Time) int64 {
 	return time.Since(start).Milliseconds()
+}
+
+// logBucket returns a string bucket label for a given positive number bucketed into
+// powers of some arbitrary base. For base 8, for example, we would have buckets:
+//
+//	<1, [1-8), [8-64), [64-512), etc.
+//
+// Go only supports a few bases with math.Log*, so this func performs a change of base:
+// log_b(x) = log_a(x) / log_a(b)
+func logBucket(base int, num int64) string {
+	if num < 1 {
+		return "<1"
+	}
+	b := float64(base)
+	logB := math.Log(float64(num)) / math.Log(b)
+	bucketExpLo := math.Floor(logB)
+	return fmt.Sprintf("[%d,%d)", int(math.Pow(b, bucketExpLo)), int(math.Pow(b, bucketExpLo+1)))
 }
