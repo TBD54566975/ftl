f1ec7dfa583b11ad39ea22292fe8e4f11f1aaa22
Matt Toohey <mtoohey@block.xyz>
2024-08-08 15:53:44 +1000

    fix: do not ask to allow incoming connections on macOS (#2298)
    macOS prompts for permission if we do not specify `127.0.0.1`

--- cmd/ftl/cmd_serve.go
+++ cmd/ftl/cmd_serve.go
@@ -36,7 +36,7 @@ import (
 )
 
 type serveCmd struct {
-	Bind                *url.URL             `help:"Starting endpoint to bind to and advertise to. Each controller, ingress and runner will increment the port by 1" default:"http://localhost:8891"`
+	Bind                *url.URL             `help:"Starting endpoint to bind to and advertise to. Each controller, ingress and runner will increment the port by 1" default:"http://127.0.0.1:8891"`
 	DBPort              int                  `help:"Port to use for the database." default:"15432"`
 	Recreate            bool                 `help:"Recreate the database even if it already exists." default:"false"`
 	Controllers         int                  `short:"c" help:"Number of controllers to start." default:"1"`
