716e6667a1077ca9161f1a4baa62bee9534525f3
Alec Thomas <aat@tbd.email>
2024-08-11 13:54:39 +1000

    feat: add support for code and SQL based database migrations (#2311)
    We'll need to be doing data migrations quite a bit in the near future,
    and while at some point we will need to do proper [zero downtime
    migrations](https://teamplify.com/blog/zero-downtime-DB-migrations/)
    with all that this entails, for now it will be more expedient to accept
    some downtime. With that in mind, this PR adds a function for applying a
    mix of SQL and code-based migrations to a database.
    
    An example of where this will be used is our encrypted table columns,
    which are currently JSONB with a base-64 encoded JSON envelope which we
    want to move to raw encrypted bytes. We might also need to re-encrypt
    some data with new keys soon. etc.
    
    Note: this is not hooked up to `ftl migrate` or `ftl dev`'s migration
    subsystems yet.
    
    ---------
    
    Co-authored-by: gak <gak@gak0.com>

--- backend/controller/sql/sqltest/testing.go
+++ backend/controller/sql/sqltest/testing.go
@@ -20,12 +20,13 @@ func OpenForTesting(ctx context.Context, t testing.TB) *sql.DB {
 	t.Helper()
 	// Acquire lock for this DB.
 	lockPath := filepath.Join(os.TempDir(), "ftl-db-test.lock")
-	release, err := flock.Acquire(ctx, lockPath, 10*time.Second)
+	release, err := flock.Acquire(ctx, lockPath, 20*time.Second)
 	assert.NoError(t, err)
 	t.Cleanup(func() { _ = release() }) //nolint:errcheck
 
 	testDSN := "postgres://localhost:15432/ftl-test?user=postgres&password=secret&sslmode=disable"
 	conn, err := databasetesting.CreateForDevel(ctx, testDSN, true)
 	assert.NoError(t, err)
+	t.Cleanup(func() { assert.NoError(t, conn.Close()) })
 	return conn
 }
