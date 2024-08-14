c64faf4999b901db158aadea62f66f03bf6faa8a
gak <gak@gak0.com>
2024-08-14 11:23:07 +1000

    fix: missed this one

--- backend/controller/cronjobs/cronjobs_integration_test.go
+++ backend/controller/cronjobs/cronjobs_integration_test.go
@@ -9,13 +9,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/alecthomas/assert/v2"
+	"github.com/alecthomas/types/optional"
+	"github.com/benbjohnson/clock"
+
 	db "github.com/TBD54566975/ftl/backend/controller/cronjobs/dal"
 	parentdb "github.com/TBD54566975/ftl/backend/controller/dal"
 	"github.com/TBD54566975/ftl/backend/controller/sql/sqltest"
 	in "github.com/TBD54566975/ftl/integration"
 	"github.com/TBD54566975/ftl/internal/log"
-	"github.com/alecthomas/assert/v2"
-	"github.com/benbjohnson/clock"
 )
 
 func TestServiceWithRealDal(t *testing.T) {
@@ -26,7 +28,7 @@ func TestServiceWithRealDal(t *testing.T) {
 
 	conn := sqltest.OpenForTesting(ctx, t)
 	dal := db.New(conn)
-	parentDAL, err := parentdb.New(ctx, conn, parentdb.NoOpEncryptors())
+	parentDAL, err := parentdb.New(ctx, conn, optional.None[string]())
 	assert.NoError(t, err)
 
 	// Using a real clock because real db queries use db clock
