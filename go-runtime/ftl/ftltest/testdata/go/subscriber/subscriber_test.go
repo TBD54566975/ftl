c6d889c82aa846c1f98a53c86861e7b63dcc6e56
Jon Johnson <113393155+jonathanj-square@users.noreply.github.com>
2024-08-14 20:35:37 -0700

    fix: addresses event completion race condition in pubsub test utility  (#2332)
    fixes: #2266
    
    The race conditions was caused by two issues.
    
    1. The WaitGroup down tick was not synchronized with the completion of
    the subscriber verb execution. Failure to wait for completion may result
    in a down time coming before a new event is dispatched (which ticks up
    the waitgroup) - which may result in the WaitGroup reaching zero too
    soon.
    2. Non-linear pubsub networks are not supported by simply counting up
    when an event is published and down after it is completed. The up count
    needs to match the number of live subscriptions (e.g. subscriptions with
    registered subscribers)
    
    This synchronization scheme does not cover asynchronous event dispatch
    (e.g. events dispatched via go routines within the subscriber verb) such
    scenarios introduce a race condition that cannot be resolved by black
    box external synchronization.

--- go-runtime/ftl/ftltest/testdata/go/subscriber/subscriber_test.go
+++ go-runtime/ftl/ftltest/testdata/go/subscriber/subscriber_test.go
@@ -2,17 +2,20 @@ package subscriber_test
 
 import (
 	"ftl/pubsub"
-	"testing"
-
 	"github.com/TBD54566975/ftl/go-runtime/ftl/ftltest"
 	"github.com/alecthomas/assert/v2"
+	"testing"
 )
 
 func TestPublishToExternalModule(t *testing.T) {
 	ctx := ftltest.Context()
-	assert.NoError(t, pubsub.Topic.Publish(ctx, pubsub.Event{Value: "external"}))
-	assert.Equal(t, 1, len(ftltest.EventsForTopic(ctx, pubsub.Topic)))
+
+	assert.NoError(t, pubsub.Topic1.Publish(ctx, pubsub.Event{Value: "external"}))
+
+	ftltest.WaitForSubscriptionsToComplete(ctx)
+
+	assert.Equal(t, 1, len(ftltest.EventsForTopic(ctx, pubsub.Topic1)))
 
 	// Make sure we correctly made the right ref for the external module.
-	assert.Equal(t, "pubsub", pubsub.Topic.Ref.Module)
+	assert.Equal(t, "pubsub", pubsub.Topic1.Ref.Module)
 }
