851171e5147ece88669539dd324272d6f5ea10b0
Safeer Jiwan <safeer@tbd.email>
2024-08-23 15:32:48 -0700

    broken test to catch transient cron origin parse errors

--- backend/controller/dal/async_calls_test.go
+++ backend/controller/dal/async_calls_test.go
@@ -22,3 +22,20 @@ func TestNoCallToAcquire(t *testing.T) {
 	assert.IsError(t, err, dalerrs.ErrNotFound)
 	assert.EqualError(t, err, "no pending async calls: not found")
 }
+
+func TestParseAsyncOrigin(t *testing.T) {
+	cronKeys := []string{
+		"crn-cron-cron-10pvs393nkv3new4",         // 1:23: exponent has no digits
+		"crn-initial-verb0-3poj0hr6wmtvmz99",     // 1:26: exponent has no digits
+		"crn-initial-verb0Cron-5eq2ivpmuv0lvnoc", // 1:30: exponent has no digits
+	}
+	for _, cronKey := range cronKeys {
+		origin, err := ParseAsyncOrigin("cron:" + cronKey)
+		assert.NoError(t, err)
+		assert.Equal(t, "cron", origin.Origin())
+
+		asyncOrigin, ok := origin.(*AsyncOriginCron)
+		assert.True(t, ok, "origin is not a cron origin")
+		assert.Equal(t, cronKey, asyncOrigin.CronJobKey)
+	}
+}
