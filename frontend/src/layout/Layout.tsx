0106f6c5aaef1e6c41791a2f116714286de2b62f
Wes <wesbillman@users.noreply.github.com>
2024-08-28 14:05:31 -0700

    feat: show real version in console nav (#2526)
    Fixes #2488
    
    ![Screenshot 2024-08-28 at 12 14
    03 PM](https://github.com/user-attachments/assets/b9fe7e81-5b8d-4fab-98ae-79007b992550)

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import { useStatus } from '../api/status/use-status'
 import { Navigation } from './navigation/Navigation'
 
 export const Layout = () => {
+  const status = useStatus()
+  const version = status.data?.controllers?.[0]?.version
+
   return (
     <div className='min-w-[800px] min-h-[600px] max-w-full max-h-full h-full flex flex-col dark:bg-gray-800 bg-white text-gray-700 dark:text-gray-200'>
-      <Navigation />
+      <Navigation version={version} />
       <main className='flex-1'>
         <Outlet />
       </main>
