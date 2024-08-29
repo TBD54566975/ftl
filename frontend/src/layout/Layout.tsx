947cdff93c7f2b03b3f7a51d9b952c175438fa77
Denise Li <dli@squareup.com>
2024-08-29 19:17:33 -0400

    feat: make the module tree scrollable + scroll to selected module (#2557)
    Final part of https://github.com/TBD54566975/ftl/issues/2487
    
    
    
    https://github.com/user-attachments/assets/eb7ed1eb-d8ae-4a92-b56e-7bde8aed3a6b

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -7,9 +7,9 @@ export const Layout = () => {
   const version = status.data?.controllers?.[0]?.version
 
   return (
-    <div className='min-w-[800px] min-h-[600px] max-w-full max-h-full h-full flex flex-col dark:bg-gray-800 bg-white text-gray-700 dark:text-gray-200'>
+    <div className='min-w-[800px] max-w-full max-h-full h-full flex flex-col dark:bg-gray-800 bg-white text-gray-700 dark:text-gray-200'>
       <Navigation version={version} />
-      <main className='flex-1'>
+      <main className='flex-1' style={{ height: 'calc(100vh - 64px)' }}>
         <Outlet />
       </main>
     </div>
