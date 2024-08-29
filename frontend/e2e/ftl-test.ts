ae78a6149df2de9ad431645cb0abacb1d3657fb9
Wes <wesbillman@users.noreply.github.com>
2024-08-29 13:14:24 -0700

    fix: biome was ignoring the e2e folder

--- frontend/e2e/ftl-test.ts
+++ frontend/e2e/ftl-test.ts
@@ -1,20 +1,19 @@
 import { test as base, expect } from '@playwright/test'
 
 const ftlTest = base.extend<{
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  // biome-ignore lint/suspicious/noExplicitAny: any is what playwright expects
   page: any
 }>({
   page: async ({ page }, use) => {
     await page.goto('http://localhost:8892/modules')
     await page.waitForFunction(() => {
-      const timeItem = document.querySelector('li#module-tree-module-time');
-      const echoItem = document.querySelector('li#module-tree-module-echo');
-      return timeItem !== null && echoItem !== null;
-    });
+      const timeItem = document.querySelector('li#module-tree-module-time')
+      const echoItem = document.querySelector('li#module-tree-module-echo')
+      return timeItem !== null && echoItem !== null
+    })
 
     await use(page)
   },
 })
 
 export { ftlTest, expect }
-
--- frontend/e2e/infrastructure.spec.ts
+++ frontend/e2e/infrastructure.spec.ts
@@ -5,15 +5,15 @@ ftlTest('shows infrastructure', async ({ page }) => {
   await infrastructureNavItem.click()
   await expect(page).toHaveURL(/\/infrastructure$/)
 
-    const controllersTab = await page.getByRole('button', { name: 'Controllers' });
-    await expect(controllersTab).toBeVisible();
+  const controllersTab = await page.getByRole('button', { name: 'Controllers' })
+  await expect(controllersTab).toBeVisible()
 
-    const runnersTab = await page.getByRole('button', { name: 'Runners' });
-    await expect(runnersTab).toBeVisible();
+  const runnersTab = await page.getByRole('button', { name: 'Runners' })
+  await expect(runnersTab).toBeVisible()
 
-    const deploymentsTab = await page.getByRole('button', { name: 'Deployments' });
-    await expect(deploymentsTab).toBeVisible();
+  const deploymentsTab = await page.getByRole('button', { name: 'Deployments' })
+  await expect(deploymentsTab).toBeVisible()
 
-    const routesTab = await page.getByRole('button', { name: 'Routes' });
-    await expect(routesTab).toBeVisible();
+  const routesTab = await page.getByRole('button', { name: 'Routes' })
+  await expect(routesTab).toBeVisible()
 })
