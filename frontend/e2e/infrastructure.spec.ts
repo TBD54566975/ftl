6eabae2d283142ea380a80a17032f0f998b103ef
Wes <wesbillman@users.noreply.github.com>
2024-08-30 09:36:24 -0700

    feat: add infrastructure tabs and state (#2553)
    See #2552
    ![Screenshot 2024-08-29 at 12 21
    36 PM](https://github.com/user-attachments/assets/ce7060d5-9425-478c-8919-69263fa12857)
    ![Screenshot 2024-08-29 at 12 23
    22 PM](https://github.com/user-attachments/assets/76517b5d-1ff3-4d6a-876b-5f2a61d1e88b)
    
    Also created new `Tabs` and `Pill` components
    
    ![Screenshot 2024-08-29 at 12 15
    51 PM](https://github.com/user-attachments/assets/432189e3-3051-470c-afc6-96bafb895d3c)
    ![Screenshot 2024-08-29 at 12 15
    56 PM](https://github.com/user-attachments/assets/7b48f7df-3c18-4822-a676-d5270428357c)
    ![Screenshot 2024-08-29 at 12 16
    00 PM](https://github.com/user-attachments/assets/71aaf858-e3af-4cb5-8f5e-80cb864e7844)

---
+++ frontend/e2e/infrastructure.spec.ts
@@ -0,0 +1,19 @@
+import { expect, ftlTest } from './ftl-test'
+
+ftlTest('shows infrastructure', async ({ page }) => {
+  const infrastructureNavItem = page.getByRole('link', { name: 'Infrastructure' })
+  await infrastructureNavItem.click()
+  await expect(page).toHaveURL(/\/infrastructure$/)
+
+  const controllersTab = await page.getByRole('button', { name: 'Controllers' })
+  await expect(controllersTab).toBeVisible()
+
+  const runnersTab = await page.getByRole('button', { name: 'Runners' })
+  await expect(runnersTab).toBeVisible()
+
+  const deploymentsTab = await page.getByRole('button', { name: 'Deployments' })
+  await expect(deploymentsTab).toBeVisible()
+
+  const routesTab = await page.getByRole('button', { name: 'Routes' })
+  await expect(routesTab).toBeVisible()
+})
---
+++ frontend/src/features/infrastructure/DeploymentsList.tsx
@@ -0,0 +1,3 @@
+export const DeploymentsList = () => {
+  return <>Deployments Content</>
+}
