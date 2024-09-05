36b43f6bfaf78f4731b9a7d4935a755e34953aae
Wes <wesbillman@users.noreply.github.com>
2024-09-05 13:59:03 -0700

    fix: make search input much more subtle

--- frontend/console/src/layout/navigation/SearchInput.tsx
+++ frontend/console/src/layout/navigation/SearchInput.tsx
@@ -32,15 +32,15 @@ export const SearchInput: React.FC<SearchInputProps> = ({ onFocus }) => {
         </label>
         <div
           id='command-palette-search'
-          className='relative block w-full cursor-pointer rounded-md border border-gray-300 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300/10 placeholder:text-gray-400 sm:text-sm sm:leading-6 hover:border-indigo-500 hover:ring-indigo-500 focus-within:border-indigo-500 focus-within:ring-indigo-600'
+          className='relative block w-full cursor-pointer rounded-md border border-indigo-700 bg-indigo-700/50 py-1.5 pl-10 pr-3 text-indigo-200  placeholder:text-indigo-300 sm:text-sm sm:leading-6 hover:border-indigo-500 hover:ring-indigo-500 focus-within:border-indigo-400 focus-within:ring-indigo-400'
           onClick={onFocus}
         >
           <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-            <Search01Icon aria-hidden='true' className='h-5 w-5 text-gray-400' />
+            <Search01Icon aria-hidden='true' className='h-5 w-5 text-indigo-300' />
           </div>
-          <span className='text-gray-400'>Search</span>
+          <span className='text-indigo-300'>Search</span>
           <div className='absolute inset-y-0 right-0 flex items-center pr-3'>
-            <span className='text-gray-400 text-xs bg-gray-100 px-2 py-1 rounded-md'>{shortcutText}</span>
+            <span className='text-indigo-200 text-xs bg-indigo-600 px-2 py-1 rounded-md'>{shortcutText}</span>
           </div>
         </div>
       </div>
