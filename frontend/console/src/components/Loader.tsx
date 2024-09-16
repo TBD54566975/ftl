TBD54566975/ftl

commit d5c6a824b71ea6a6d0e389f4fe467bb0158d8fd6

chore(deps): update all non-major dependencies (#2676)
This PR contains the following updates:

| Package | Change | Age | Adoption | Passing | Confidence | Type |
Update |
|---|---|---|---|---|---|---|---|
|
[@chromatic-com/storybook](https://redirect.github.com/chromaui/addon-visual-tests)
| [`1.7.0` ->
`1.9.0`](https://renovatebot.com/diffs/npm/@chromatic-com%2fstorybook/1.7.0/1.9.0)
|
[![age](https://developer.mend.io/api/mc/badges/age/npm/@chromatic-com%2fstorybook/1.9.0?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![adoption](https://developer.mend.io/api/mc/badges/adoption/npm/@chromatic-com%2fstorybook/1.9.0?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![passing](https://developer.mend.io/api/mc/badges/compatibility/npm/@chromatic-com%2fstorybook/1.7.0/1.9.0?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![confidence](https://developer.mend.io/api/mc/badges/confidence/npm/@chromatic-com%2fstorybook/1.7.0/1.9.0?slim=true)](https://docs.renovatebot.com/merge-confidence/)
| devDependencies | minor |
|
[@codemirror/autocomplete](https://redirect.github.com/codemirror/autocomplete)
| [`6.18.0` ->
`6.18.1`](https://renovatebot.com/diffs/npm/@codemirror%2fautocomplete/6.18.0/6.18.1)
|
[![age](https://developer.mend.io/api/mc/badges/age/npm/@codemirror%2fautocomplete/6.18.1?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![adoption](https://developer.mend.io/api/mc/badges/adoption/npm/@codemirror%2fautocomplete/6.18.1?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![passing](https://developer.mend.io/api/mc/badges/compatibility/npm/@codemirror%2fautocomplete/6.18.0/6.18.1?slim=true)](https://docs.renovatebot.com/merge-confidence/)
|
[![confidence](https://developer.mend.io/api/mc/badges/confidence/npm/@codemirror%2fautocomplete/6.18.0/6.18.1?slim=true)](https://docs.renovatebot.com/merge-confidence/)
| dependencies | patch |
|

frontend/console/src/components/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div role='status'>
+    <output>
       <svg
         aria-hidden='true'
         className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
@@ -18,6 +18,6 @@ export const Loader = () => {
         />
       </svg>
       <span className='sr-only'>Loading...</span>
-    </div>
+    </output>
   )
 }

frontend/console/src/layout/navigation/SearchInput.tsx
@@ -27,7 +27,9 @@ export const SearchInput: React.FC<SearchInputProps> = ({ onFocus }) => {
   return (
     <div className='flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end'>
       <div className='w-full max-w-lg lg:max-w-xs'>
-        <label className='sr-only'>Search</label>
+        <label htmlFor='command-palette-search' className='sr-only'>
+          Search
+        </label>
         <div
           id='command-palette-search'
           className='relative block w-full cursor-pointer rounded-md border border-indigo-700 bg-indigo-700/50 py-1.5 pl-10 pr-3 text-indigo-200  placeholder:text-indigo-300 sm:text-sm sm:leading-6 hover:border-indigo-500 hover:ring-indigo-500 focus-within:border-indigo-400 focus-within:ring-indigo-400'

commit 4ab53ccb2cb55e32c5fbbc56dd86914edee6280e

chore: remove some screenreader support that had upset the linter (#2696)
Removes the 2 `sr-only` elements that caused lint errors in
https://github.com/TBD54566975/ftl/pull/2676. The changes suggested by
the linter weren't really quite exactly correct either, and we aren't
formally supporting screenreaders in the console anyway, so we can just
remove it.

frontend/console/src/components/Loader.tsx
@@ -1,23 +1,20 @@
 export const Loader = () => {
   return (
-    <output>
-      <svg
-        aria-hidden='true'
-        className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
-        viewBox='0 0 100 101'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path
-          d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-          fill='currentColor'
-        />
-        <path
-          d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-          fill='currentFill'
-        />
-      </svg>
-      <span className='sr-only'>Loading...</span>
-    </output>
+    <svg
+      aria-hidden='true'
+      className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+      viewBox='0 0 100 101'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+        fill='currentColor'
+      />
+      <path
+        d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+        fill='currentFill'
+      />
+    </svg>
   )
 }

frontend/console/src/layout/navigation/SearchInput.tsx
@@ -27,9 +27,6 @@ export const SearchInput: React.FC<SearchInputProps> = ({ onFocus }) => {
   return (
     <div className='flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end'>
       <div className='w-full max-w-lg lg:max-w-xs'>
-        <label htmlFor='command-palette-search' className='sr-only'>
-          Search
-        </label>
         <div
           id='command-palette-search'
           className='relative block w-full cursor-pointer rounded-md border border-indigo-700 bg-indigo-700/50 py-1.5 pl-10 pr-3 text-indigo-200  placeholder:text-indigo-300 sm:text-sm sm:leading-6 hover:border-indigo-500 hover:ring-indigo-500 focus-within:border-indigo-400 focus-within:ring-indigo-400'
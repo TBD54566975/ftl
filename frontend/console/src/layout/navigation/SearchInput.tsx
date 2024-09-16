4ab53ccb2cb55e32c5fbbc56dd86914edee6280e
Denise Li <dli@squareup.com>
2024-09-16 16:29:35 -0400

    chore: remove some screenreader support that had upset the linter (#2696)
    Removes the 2 `sr-only` elements that caused lint errors in
    https://github.com/TBD54566975/ftl/pull/2676. The changes suggested by
    the linter weren't really quite exactly correct either, and we aren't
    formally supporting screenreaders in the console anyway, so we can just
    remove it.

--- frontend/console/src/layout/navigation/SearchInput.tsx
+++ frontend/console/src/layout/navigation/SearchInput.tsx
@@ -27,9 +27,6 @@ export const SearchInput: React.FC<SearchInputProps> = ({ onFocus }) => {
   return (
     <div className='flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end'>
       <div className='w-full max-w-lg lg:max-w-xs'>
-        <label htmlFor='command-palette-search' className='sr-only'>
-          Search
-        </label>
         <div
           id='command-palette-search'
           className='relative block w-full cursor-pointer rounded-md border border-indigo-700 bg-indigo-700/50 py-1.5 pl-10 pr-3 text-indigo-200  placeholder:text-indigo-300 sm:text-sm sm:leading-6 hover:border-indigo-500 hover:ring-indigo-500 focus-within:border-indigo-400 focus-within:ring-indigo-400'
