9003ae379cc8246d7668801e161049e54ccf76f5
Juho Mäkinen <juho@squareup.com>
2024-09-23 08:56:52 +1000

    chore: add a call through ftl-provisioner service (#2747)
    In the spirit of keeping PRs small, this sets up a simple call through
    service `ftl-provisioner` to call create deployments in
    `ftl-controller`.
    
    The idea is to extend this later to do the infra provisioning before
    calling the controller, and then at some point point deployments to this
    service instead.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ cmd/ftl-provisioner/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/alecthomas/kong"
+
+	"github.com/TBD54566975/ftl"
+	"github.com/TBD54566975/ftl/backend/provisioner"
+	_ "github.com/TBD54566975/ftl/internal/automaxprocs" // Set GOMAXPROCS to match Linux container CPU quota.
+	"github.com/TBD54566975/ftl/internal/log"
+	"github.com/TBD54566975/ftl/internal/observability"
+)
+
+var cli struct {
+	Version             kong.VersionFlag     `help:"Show version."`
+	ObservabilityConfig observability.Config `embed:"" prefix:"o11y-"`
+	LogConfig           log.Config           `embed:"" prefix:"log-"`
+	ProvisionerConfig   provisioner.Config   `embed:""`
+	ConfigFlag          string               `name:"config" short:"C" help:"Path to FTL project cf file." env:"FTL_CONFIG" placeholder:"FILE"`
+}
+
+func main() {
+	t, err := strconv.ParseInt(ftl.Timestamp, 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("invalid timestamp %q: %s", ftl.Timestamp, err))
+	}
+	kctx := kong.Parse(&cli,
+		kong.Description(`FTL - Towards a 𝝺-calculus for large-scale systems`),
+		kong.UsageOnError(),
+		kong.Vars{"version": ftl.Version, "timestamp": time.Unix(t, 0).Format(time.RFC3339)},
+	)
+	cli.ProvisionerConfig.SetDefaults()
+
+	ctx := log.ContextWithLogger(context.Background(), log.Configure(os.Stderr, cli.LogConfig))
+	err = observability.Init(ctx, false, "", "ftl-provisioner", ftl.Version, cli.ObservabilityConfig)
+	kctx.FatalIfErrorf(err, "failed to initialize observability")
+
+	err = provisioner.Start(ctx, cli.ProvisionerConfig, false)
+	kctx.FatalIfErrorf(err)
+}
