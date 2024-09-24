a30513f9155a22b7a1054f057b049bbe3fe74141
Moe Jangda <moe@tbd.email>
2024-09-24 16:44:06 -0700

    feat: instrument app DBs (#2792)
    This PR instruments app DBs to report db related metrics for application
    dbs. I verified that it's working locally
    
    <img width="1206" alt="image"
    src="https://github.com/user-attachments/assets/d4446371-cfe8-4e7e-b126-7a0f1c38bf89">
    
    > [!WARNING]
    > I haven't been able to verify whether the appropriate service name is
    being set.
    [This](https://github.com/tbd54566975/ftl/blob/main/go-runtime/server/server.go#L50)
    made me feel like it would but i'm not 100% certain yet. I need to
    figure out whether grafana treats `service.name` as a label that can be
    queried. If that is the case then `service.name` isn't being set in
    which case we'll need to provide a custom metrics provider to `otelsql`.
    We can hold off on merging this PR until i get that figured out or we
    can do it in a subsequent PR.
    >
    > FWIW, i am setting the `db_name` attribute to the appropriate db name,
    so there technically is a way to distinguish between ftl db metrics and
    app db metrics right now, though it's definitely unideal
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Safeer Jiwan <safeer@tbd.email>

--- go-runtime/ftl/database.go
+++ go-runtime/ftl/database.go
@@ -5,8 +5,11 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/XSAM/otelsql"
 	"github.com/alecthomas/types/once"
 	_ "github.com/jackc/pgx/v5/stdlib" // Register Postgres driver
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 
 	"github.com/TBD54566975/ftl/internal/modulecontext"
 )
@@ -29,9 +32,20 @@ func PostgresDatabase(name string) Database {
 			if err != nil {
 				return nil, fmt.Errorf("failed to get database %q: %w", name, err)
 			}
-			db, err := sql.Open("pgx", dsn)
+			db, err := otelsql.Open("pgx", dsn)
 			if err != nil {
 				return nil, fmt.Errorf("failed to open database %q: %w", name, err)
+			}
+
+			// sets db.system and db.name attributes
+			metricAttrs := otelsql.WithAttributes(
+				semconv.DBSystemPostgreSQL,
+				semconv.DBNameKey.String(name),
+				attribute.Bool("ftl.is_user_service", true),
+			)
+			err = otelsql.RegisterDBStatsMetrics(db, metricAttrs)
+			if err != nil {
+				return nil, fmt.Errorf("failed to register database metrics: %w", err)
 			}
 			return db, nil
 		}),
