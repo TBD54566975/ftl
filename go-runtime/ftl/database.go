009fa3db827faa27badd89185e2efd4828f38bcc
Moe Jangda <moe@tbd.email>
2024-09-24 16:35:57 -0700

    remove borked attribute

--- go-runtime/ftl/database.go
+++ go-runtime/ftl/database.go
@@ -42,7 +42,6 @@ func PostgresDatabase(name string) Database {
 				semconv.DBSystemPostgreSQL,
 				semconv.DBNameKey.String(name),
 				attribute.Bool("ftl.is_user_service", true),
-				attribute.String("ftl.sql.module.name", provider.module),
 			)
 			err = otelsql.RegisterDBStatsMetrics(db, metricAttrs)
 			if err != nil {
