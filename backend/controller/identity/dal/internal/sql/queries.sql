8e84e9a5c3a2f57764dffc6317ddebb1ddeb8f0f
gak <gak@gak0.com>
2024-09-25 10:07:27 +1000

    fix: dont rely on hardcoded primary key

--- backend/controller/identity/dal/internal/sql/queries.sql
+++ backend/controller/identity/dal/internal/sql/queries.sql
@@ -1,8 +1,7 @@
 -- name: GetOnlyIdentityKey :one
 SELECT private, public, verify_signature
-FROM identity_keys
-WHERE id = 1;
+FROM identity_keys;
 
 -- name: CreateOnlyIdentityKey :exec
-INSERT INTO identity_keys (id, private, public, verify_signature)
-VALUES (1, $1, $2, $3);
+INSERT INTO identity_keys (private, public, verify_signature)
+VALUES ($1, $2, $3);
