8409a1ef951ed8601283c89132a1fe425a63e067
Elizabeth Worstell <worstell@squareup.com>
2024-09-30 16:47:05 -0700

    feat: inject DB verb resources

--- go-runtime/schema/testdata/failing/failing.go
+++ go-runtime/schema/testdata/failing/failing.go
@@ -17,8 +17,11 @@ var goodConfig = ftl.Config[string]("FTL_CONFIG_ENDPOINT")
 // var duplSecret = ftl.Secret[string]("FTL_ENDPOINT")
 var goodSecret = ftl.Secret[string]("FTL_SECRET_ENDPOINT")
 
-// var duplDB = ftl.PostgresDatabase("testDb")
-var goodDB = ftl.PostgresDatabase("testDb")
+type DuplDbConfig struct {
+	ftl.DefaultPostgresDatabaseConfig
+}
+
+func (DuplDbConfig) Name() string { return "testdb" }
 
 type Request struct {
 	BadParam error
