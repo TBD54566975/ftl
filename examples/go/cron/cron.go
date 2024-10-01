d7dc0b0726d1d2f007a4aff03e1c835dc3bd4896
Safeer Jiwan <safeer@tbd.email>
2024-10-01 15:34:05 -0700

    feat: add cron go+java examples (#2930)

---
+++ examples/go/cron/cron.go
@@ -0,0 +1,19 @@
+package cron
+
+import (
+	"context"
+
+	"github.com/TBD54566975/ftl/go-runtime/ftl" // Import the FTL SDK.
+)
+
+//ftl:cron 30s
+func ThirtySeconds(ctx context.Context) error {
+	ftl.LoggerFromContext(ctx).Infof("Frequent cron job triggered.")
+	return nil
+}
+
+//ftl:cron 0 * * * *
+func Hourly(ctx context.Context) error {
+	ftl.LoggerFromContext(ctx).Infof("Hourly cron job triggered.")
+	return nil
+}
