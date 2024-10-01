9fc18728bd2ac7477102c091c84795b30dd6d3af
Tom Daffurn <tdaffurn@tbd.email>
2024-10-01 10:48:31 +1000

    Validate enums field and type

--- jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/EnumProcessor.java
+++ jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/EnumProcessor.java
@@ -1,14 +1,18 @@
 package xyz.block.ftl.deployment;
 
+import static org.jboss.jandex.PrimitiveType.Primitive.BYTE;
+import static org.jboss.jandex.PrimitiveType.Primitive.INT;
+import static org.jboss.jandex.PrimitiveType.Primitive.LONG;
+import static org.jboss.jandex.PrimitiveType.Primitive.SHORT;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
-import org.jboss.jandex.PrimitiveType;
+import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +37,7 @@ public class EnumProcessor {
         log.info("Processing {} enum annotations into build items", enumAnnotations.size());
         List<Decl> decls = new ArrayList<>();
         try {
+            // TODO how do we exclude @Enum annotations from generated verb clients?
             for (var enumAnnotation : enumAnnotations) {
                 boolean exported = enumAnnotation.target().hasAnnotation(FTLDotNames.EXPORT);
                 ClassInfo enumClassInfo = enumAnnotation.target().asClass();
@@ -41,12 +46,19 @@ public class EnumProcessor {
                         .setExport(exported);
                 if (enumClassInfo.isEnum()) {
                     // Value enums must have a type
-                    Type type = enumClassInfo.field("value").type();
+                    FieldInfo valueField = enumClassInfo.field("value");
+                    if (valueField == null) {
+                        throw new RuntimeException("Enum must have a 'value' field: " + enumClassInfo.name());
+                    }
+                    Type type = valueField.type();
                     xyz.block.ftl.v1.schema.Type.Builder typeBuilder = xyz.block.ftl.v1.schema.Type.newBuilder();
-                    if (type == PrimitiveType.LONG) {
+                    if (isInt(type)) {
                         typeBuilder.setInt(Int.newBuilder().build()).build();
-                    } else {
+                    } else if (type.name().equals(DotName.STRING_NAME)) {
                         typeBuilder.setString(xyz.block.ftl.v1.schema.String.newBuilder().build());
+                    } else {
+                        throw new RuntimeException(
+                                "Enum value type must be String, int, long, short, or byte: " + enumClassInfo.name());
                     }
                     enumBuilder.setType(typeBuilder.build());
 
@@ -56,14 +68,12 @@ public class EnumProcessor {
                         Field value = constant.getClass().getDeclaredField("value");
                         value.setAccessible(true);
                         Value.Builder valueBuilder = Value.newBuilder();
-                        if (type == PrimitiveType.LONG) {
+                        if (isInt(type)) {
                             long aLong = value.getLong(constant);
                             valueBuilder.setIntValue(IntValue.newBuilder().setValue(aLong).build());
-                        } else if (type.name().equals(DotName.STRING_NAME)) {
+                        } else {
                             String aString = (String) value.get(constant);
                             valueBuilder.setStringValue(StringValue.newBuilder().setValue(aString).build());
-                        } else {
-                            throw new RuntimeException("Unsupported enum value type: " + value.getType());
                         }
                         EnumVariant variant = EnumVariant.newBuilder()
                                 .setName(constant.toString())
@@ -83,6 +93,13 @@ public class EnumProcessor {
         } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private boolean isInt(Type type) {
+        if (type.kind() != Type.Kind.PRIMITIVE) {
+            return false;
+        }
+        return Set.of(INT, LONG, BYTE, SHORT).contains(type.asPrimitiveType().primitive());
     }
 
 }
