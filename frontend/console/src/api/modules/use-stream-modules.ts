11fab8358dac9eb067d1f031275a2cab588f599d
Denise Li <dli@squareup.com>
2024-10-01 15:43:24 -0400

    use

---
+++ frontend/console/src/api/modules/use-stream-modules.ts
@@ -0,0 +1,49 @@
+import { Code, ConnectError } from '@connectrpc/connect'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useClient } from '../../hooks/use-client'
+import { useVisibility } from '../../hooks/use-visibility'
+import { ConsoleService } from '../../protos/xyz/block/ftl/v1/console/console_connect'
+import type { Module } from '../../protos/xyz/block/ftl/v1/console/console_pb'
+
+const streamModulesKey = 'streamModules'
+const updateIntervalMs = 60 * 1000
+
+export const useStreamModules = () => {
+  const client = useClient(ConsoleService)
+  const queryClient = useQueryClient()
+  const isVisible = useVisibility()
+
+  const queryKey = [streamModulesKey]
+
+  const streamModules = async ({ signal }: { signal: AbortSignal }) => {
+    try {
+      console.debug('streaming modules')
+      for await (const response of client.streamModules(
+        { updateInterval: { seconds: BigInt(0), nanos: updateIntervalMs * 1000 }, query: {} },
+        { signal },
+      )) {
+        console.debug('stream-modules-response:', response)
+        if (response.modules) {
+          const newModuleNames = response.modules.map((m) => m.name)
+          queryClient.setQueryData<Module[]>(queryKey, (prev = []) => {
+            return [...response.modules, ...prev.filter((m) => !newModuleNames.includes(m.name))]
+          })
+        }
+      }
+    } catch (error) {
+      if (error instanceof ConnectError) {
+        if (error.code !== Code.Canceled) {
+          console.error('Console service - streamEvents - Connect error:', error)
+        }
+      } else {
+        console.error('Console service - streamEvents:', error)
+      }
+    }
+  }
+
+  return useQuery({
+    queryKey: queryKey,
+    queryFn: async ({ signal }) => streamModules({ signal }),
+    enabled: isVisible,
+  })
+}
