1d79ea761c304f94854d8af15a0ea47f7cf84feb
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-10-08 12:10:12 +1100

    feat: Improve local subscriptions

--- jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/SubscriptionMetaAnnotationsBuildItem.java
+++ jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/SubscriptionMetaAnnotationsBuildItem.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationValue;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
 
 import io.quarkus.builder.item.SimpleBuildItem;
+import xyz.block.ftl.Topic;
 
 public final class SubscriptionMetaAnnotationsBuildItem extends SimpleBuildItem {
 
@@ -23,13 +25,33 @@ public final class SubscriptionMetaAnnotationsBuildItem extends SimpleBuildItem 
     public record SubscriptionAnnotation(String module, String topic, String name) {
     }
 
-    public static SubscriptionAnnotation fromJandex(AnnotationInstance subscriptions, String currentModuleName) {
-        AnnotationValue moduleValue = subscriptions.value("module");
+    public static SubscriptionAnnotation fromJandex(IndexView indexView, AnnotationInstance subscriptions,
+            String currentModuleName) {
 
+        AnnotationValue moduleValue = subscriptions.value("module");
+        AnnotationValue topicValue = subscriptions.value("topic");
+        AnnotationValue topicClassValue = subscriptions.value("topicClass");
+        String topicName;
+        if (topicValue != null && !topicValue.asString().isEmpty()) {
+            if (topicClassValue != null && !topicClassValue.asClass().name().toString().equals(Topic.class.getName())) {
+                throw new IllegalArgumentException("Cannot specify both topic and topicClass");
+            }
+            topicName = topicValue.asString();
+        } else if (topicClassValue != null && !topicClassValue.asClass().name().toString().equals(Topic.class.getName())) {
+            var topicClass = indexView.getClassByName(topicClassValue.asClass().name());
+            AnnotationInstance annotation = topicClass.annotation(FTLDotNames.TOPIC_DEFINITION);
+            if (annotation == null) {
+                throw new IllegalArgumentException(
+                        "topicClass must be annotated with @TopicDefinition for subscription " + subscriptions);
+            }
+            topicName = annotation.value().asString();
+        } else {
+            throw new IllegalArgumentException("Either topic or topicClass must be specified on " + subscriptions);
+        }
         return new SubscriptionMetaAnnotationsBuildItem.SubscriptionAnnotation(
                 moduleValue == null || moduleValue.asString().isEmpty() ? currentModuleName
                         : moduleValue.asString(),
-                subscriptions.value("topic").asString(),
+                topicName,
                 subscriptions.value("name").asString());
     }
 }
