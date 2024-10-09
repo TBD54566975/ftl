ba72d42756d37412a9c33577f8adb0b3a96f68b5
Wes <wesbillman@users.noreply.github.com>
2024-10-09 12:14:01 -0700

    feat: new, simplified modules page (#3054)
    This page was legacy deployment stuff before so just migrating to a more
    consistent design with basic info to be expanded in the future
    
    ![Screenshot 2024-10-09 at 11 32
    44 AM](https://github.com/user-attachments/assets/8b302cf0-6670-4060-8d06-82ee3b4660b0)

---
+++ frontend/console/src/features/modules/ModulesPanel.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+import { useModules } from '../../api/modules/use-modules'
+import { AttributeBadge } from '../../components'
+import { List } from '../../components/List'
+import type { Module } from '../../protos/xyz/block/ftl/v1/console/console_pb'
+import { classNames } from '../../utils'
+import { deploymentTextColor } from '../deployments/deployment.utils'
+
+export const ModulesPanel = () => {
+  const modules = useModules()
+  const navigate = useNavigate()
+
+  const handleModuleClick = (module: Module) => {
+    navigate(`/modules/${module.name}`)
+  }
+
+  return (
+    <div className='p-2'>
+      <List
+        items={modules.data?.modules ?? []}
+        onClick={handleModuleClick}
+        renderItem={(module) => (
+          <div className='flex w-full' data-module-row={module.name}>
+            <div className='flex gap-x-4 items-center w-1/2'>
+              <div className='whitespace-nowrap'>
+                <div className='flex gap-x-2 items-center'>
+                  <p>{module.name}</p>
+                </div>
+
+                <p className={classNames(deploymentTextColor(module.deploymentKey), 'text-sm leading-6')}>{module.deploymentKey}</p>
+              </div>
+            </div>
+            <div className='flex gap-x-4 items-center w-1/2 justify-end'>
+              <div className='flex flex-wrap gap-1'>
+                <AttributeBadge name='language' value={module.language} />
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  )
+}
