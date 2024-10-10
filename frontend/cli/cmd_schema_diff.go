8609ebcfb5806836744487e610a4da86fc8c1a34
Matt Toohey <mtoohey@block.xyz>
2024-10-10 12:13:48 +1100

    feat: add external plugin to build engine
    # Conflicts:
    #	backend/protos/xyz/block/ftl/v1/language/mixins.go

--- frontend/cli/cmd_schema_diff.go
+++ frontend/cli/cmd_schema_diff.go
@@ -107,7 +107,7 @@ func localSchema(ctx context.Context, projectConfig projectconfig.Config) (*sche
 			if err != nil {
 				moduleSchemas <- either.RightOf[*schema.Module](err)
 			}
-			defer plugin.Kill(ctx) // nolint:errcheck
+			defer plugin.Kill() // nolint:errcheck
 
 			customDefaults, err := plugin.ModuleConfigDefaults(ctx, m.Dir)
 			if err != nil {
