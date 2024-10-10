09bcd0c33e52d45622933c323365866174d77c9c
Denise Li <dli@squareup.com>
2024-10-09 22:10:07 -0400

    always scroll to top when opening new module page

--- frontend/console/src/features/modules/ModulePanel.tsx
+++ frontend/console/src/features/modules/ModulePanel.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { useStreamModules } from '../../api/modules/use-stream-modules'
 import { Schema } from './schema/Schema'
@@ -6,6 +6,7 @@ import { Schema } from './schema/Schema'
 export const ModulePanel = () => {
   const { moduleName } = useParams()
   const modules = useStreamModules()
+  const ref = useRef<HTMLDivElement>(null)
 
   const module = useMemo(() => {
     if (!modules?.data) {
@@ -14,10 +15,14 @@ export const ModulePanel = () => {
     return modules.data.find((module) => module.name === moduleName)
   }, [modules?.data, moduleName])
 
+  useEffect(() => {
+    ref?.current?.parentElement?.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [moduleName])
+
   if (!module) return
 
   return (
-    <div className='mt-4 mx-4'>
+    <div ref={ref} className='mt-4 mx-4'>
       <Schema schema={module.schema} />
     </div>
   )
