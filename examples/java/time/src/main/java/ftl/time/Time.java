db0ca7fb8c78f170e69db24524fc8325f6d4c948
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-10-10 12:46:55 +1100

    feat: observability support for JVM (#3051)
    fixes: #3041

--- examples/java/time/src/main/java/ftl/time/Time.java
+++ examples/java/time/src/main/java/ftl/time/Time.java
@@ -2,14 +2,25 @@ package ftl.time;
 
 import java.time.OffsetDateTime;
 
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 import xyz.block.ftl.Export;
 import xyz.block.ftl.Verb;
 
 public class Time {
+    final LongCounter counter;
+
+    public Time(Meter meter) {
+        counter = meter.counterBuilder("time.invocations")
+                .setDescription("The number of time invocations")
+                .setUnit("invocations")
+                .build();
+    }
 
     @Verb
     @Export
-    public static TimeResponse time() {
+    public TimeResponse time() {
+        counter.add(1);
         return new TimeResponse(OffsetDateTime.now());
     }
 }
