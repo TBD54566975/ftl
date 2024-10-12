915fab9a94d100383fcc67c09298d2e9e6f82925
Alec Thomas <aat@tbd.email>
2024-10-12 13:27:44 +1100

    feat: go2proto generates To/FromProto functions

---
+++ cmd/go2proto/proto.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"text/template"
+
+	"github.com/TBD54566975/ftl/internal/schema/strcase"
+)
+
+var protoTmpl = template.Must(template.New("proto").
+	Funcs(template.FuncMap{
+		"typeof":       func(t any) string { return reflect.Indirect(reflect.ValueOf(t)).Type().Name() },
+		"toLowerCamel": strcase.ToLowerCamel,
+		"toUpperCamel": strcase.ToUpperCamel,
+		"toLowerSnake": strcase.ToLowerSnake,
+		"toUpperSnake": strcase.ToUpperSnake,
+		"trimPrefix":   strings.TrimPrefix,
+	}).
+	Parse(`
+// THIS FILE IS GENERATED; DO NOT MODIFY
+syntax = "proto3";
+
+package {{ .Package }};
+{{ range .Imports }}
+import "{{.}}";
+{{- end}}
+{{ range $name, $value := .Options }}
+option {{ $name }} = {{ $value }};
+{{ end -}}
+
+{{ range $decl := .OrderedDecls }}
+{{- if eq (typeof $decl) "Message" }}
+message {{ .Name }} {
+{{- range $name, $field := .Fields }}
+	{{ if .Repeated }}repeated {{else if .Optional}}optional {{ end }}{{ .Type }} {{ .Name | toLowerSnake }} = {{ .ID }};
+{{- end }}
+}
+{{- else if eq (typeof $decl) "Enum" }}
+enum {{ .Name }} {
+{{- range $value, $name := .ByValue }}
+	{{ $name | toUpperSnake }} = {{ $value }};
+{{- end }}
+}
+{{- else if eq (typeof $decl) "SumType" }}
+{{ $sumtype := . }}
+message {{ .Name }}  {
+	oneof value {
+{{- range $name, $id := .Elems }}
+		{{ $name }} {{ trimPrefix $name $sumtype.Name | toLowerSnake }} = {{ $id }};
+{{- end }}
+	}
+}
+{{- end }}
+{{ end }}
+`))
+
+type RenderContext struct {
+	Config
+	File
+}
+
+func render(out *os.File, config Config, file File) error {
+	err := protoTmpl.Execute(out, RenderContext{
+		Config: config,
+		File:   file,
+	})
+	if err != nil {
+		return fmt.Errorf("template error: %w", err)
+	}
+	return nil
+}
