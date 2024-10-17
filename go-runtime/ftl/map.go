4d9ad16e5c2f3b9025e02924930210ae6168ca0c
Elizabeth Worstell <worstell@squareup.com>
2024-10-17 16:44:19 -0700

    feat: support ftl.Map on injected resources

--- go-runtime/ftl/map.go
+++ go-runtime/ftl/map.go
@@ -32,3 +32,51 @@ func Map[T, U any](getter Handle[T], fn func(context.Context, T) (U, error)) *Ma
 		handle: getter,
 	}
 }
+
+// ResourceMapper is an interface that can be implemented to map a resource of type `From` to a new type `To`.
+//
+// A ResourceMapper struct should embed the Handle type that it is mapping from and define a `Map` method that
+// returns the new type.
+//
+// e.g.
+//
+//	 type DBMapper struct {
+//	   ftl.DatabaseHandle[MyConfig]
+//	 }
+//
+//		func (DBMapper) Map(ctx context.Context, conn *sql.DB) (NewType, error) {
+//		  ...
+//		}
+type ResourceMapper[From, To any] interface {
+	Handle[From]
+	Map(ctx context.Context, f From) (To, error)
+}
+
+// MappedHandle can be used to provide the mapped value of a resource in a verb signature.
+//
+// e.g.
+// //ftl:verb
+//
+//	func MyVerb(ctx context.Context, req Request, handle ftl.MappedHandle[DBMapper, *sql.DB, NewType]) (Response, error) {
+//	  db := handle.Get(ctx)
+//	  ...
+//	}
+type MappedHandle[R ResourceMapper[From, To], From, To any] struct {
+	mapper R
+}
+
+func (mh *MappedHandle[H, From, To]) Get(ctx context.Context) To {
+	value := mh.mapper.Get(ctx)
+	out := internal.FromContext(ctx).CallMap(ctx, mh, value, func(ctx context.Context) (any, error) {
+		return mh.mapper.Map(ctx, value)
+	})
+	u, ok := out.(To)
+	if !ok {
+		panic(fmt.Sprintf("output object %v is not compatible with expected type %T", out, *new(To)))
+	}
+	return u
+}
+
+func NewMappedHandle[H ResourceMapper[From, To], From, To any](mapper H) MappedHandle[H, From, To] {
+	return MappedHandle[H, From, To]{mapper}
+}
