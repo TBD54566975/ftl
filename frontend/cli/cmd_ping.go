16e661af357bdb804ebd8360ea5bb93755a6cb97
Matt Toohey <mtoohey@block.xyz>
2024-10-18 09:46:36 +1100

    lint

--- frontend/cli/cmd_ping.go
+++ frontend/cli/cmd_ping.go
@@ -15,5 +15,5 @@ type pingCmd struct {
 }
 
 func (c *pingCmd) Run(ctx context.Context, controller ftlv1connect.ControllerServiceClient) error {
-	return rpc.Wait(ctx, backoff.Backoff{Max: time.Second}, c.Wait, controller)
+	return rpc.Wait(ctx, backoff.Backoff{Max: time.Second}, c.Wait, controller) //nolint:wrapcheck
 }
