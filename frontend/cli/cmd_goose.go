9769a1ef9b3ec981ebcd8583f91f0d9852bf5671
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-10-24 16:37:22 +1100

    tmp

--- frontend/cli/cmd_goose.go
+++ frontend/cli/cmd_goose.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"strings"
 
 	"github.com/TBD54566975/ftl/internal/exec"
@@ -21,20 +22,31 @@ func (c *gooseCmd) Run(ctx context.Context) error {
 	args := []string{"goose", "run"}
 	if first {
 		first = false
-		data = "You are working with a system called FTL within an existing project, the docs are at https://github.com/TBD54566975/ftl/tree/main/docs/content/docs/reference. You can verify that the project builds via ftl build. Files that end in .ftl.go are auto generated and must not be modified." + data
+		data = "You are working with a system called FTL within an existing project, the docs are at https://github.com/TBD54566975/ftl/tree/main/docs/content/docs/reference. You can verify that the project builds via ftl build. Files that end in .ftl.go are auto generated and must not be modified. You can interact with the FTL cluster using the FTL command, run ftl --help for more details. " + data
 	} else {
 		args = append(args, "--resume-session")
 	}
 	terminal.UpdateModuleState(ctx, "FTL - Goose", terminal.BuildStateBuilding)
 	cmd := exec.Command(ctx, log.Debug, ".", "sq", args...)
 	cmd.Stdin = strings.NewReader(data)
-	cmd.Stdout = nil
-	cmd.Stderr = nil
-	capture, err := cmd.CombinedOutput()
-	fmt.Printf("%s\n", capture)
+	out := &output{}
+
+	cmd.Stdout = out
+	cmd.Stderr = out
+	err := cmd.Run()
 	terminal.UpdateModuleState(ctx, "FTL - Goose", terminal.BuildStateTerminated)
 	if err != nil {
 		return fmt.Errorf("goose failed: %w", err)
 	}
 	return nil
+}
+
+var _ io.Writer = &output{}
+
+type output struct {
+}
+
+func (o output) Write(p []byte) (n int, err error) {
+	fmt.Printf("%s", string(p))
+	return len(p), nil
 }
