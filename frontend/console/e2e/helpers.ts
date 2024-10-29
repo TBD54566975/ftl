3bebd2466d5971b04321fa183621d32051147e4b
Denise Li <dli@squareup.com>
2024-10-29 18:55:47 -0400

    feat: hide unexported decls in console by default and provide a toggle to show (#3236)
    Part 1 of https://github.com/TBD54566975/ftl/issues/3219
    
    
    https://github.com/user-attachments/assets/5cb024af-4d59-4a6a-b7ca-678ad1001fed
    
    <img width="180" alt="Screenshot 2024-10-29 at 4 37 48 PM"
    src="https://github.com/user-attachments/assets/b4ed15ff-1958-45e1-b6fb-0cb2a6203013">

--- frontend/console/e2e/helpers.ts
+++ frontend/console/e2e/helpers.ts
@@ -15,6 +15,10 @@ export async function navigateToModule(page: Page, moduleName: string) {
 export async function navigateToDecl(page: Page, moduleName: string, declName: string) {
   await navigateToModule(page, moduleName)
 
+  // Some decls are hidden by default because they are not exported, so click
+  // the toggle to make them visible.
+  await page.locator('#hide-exported').click()
+
   // Navigate to the decl page
   await page.locator(`a#decl-${declName}`).click()
   await expect(page).toHaveURL(new RegExp(`/modules/${moduleName}/verb/${declName}`))
