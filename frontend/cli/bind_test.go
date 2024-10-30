79cc787b83cec6cb3c6efbae706e1e172c4d3323
Matt Toohey <mtoohey@block.xyz>
2024-10-30 13:33:55 +1100

    lint

--- frontend/cli/bind_test.go
+++ frontend/cli/bind_test.go
@@ -16,10 +16,10 @@ func TestBindLocalWithRemoteEndpoint(t *testing.T) {
 	bindAllocator, err := bindAllocatorWithoutController()
 	assert.NoError(t, err)
 
-	nextUrl, err := bindAllocator.Next()
+	nextURL, err := bindAllocator.Next()
 	assert.NoError(t, err)
 
-	assert.True(t, strings.HasPrefix(nextUrl.String(), "http://127.0.0.1:"), nextUrl.String())
+	assert.True(t, strings.HasPrefix(nextURL.String(), "http://127.0.0.1:"), nextURL.String())
 
 	bindAllocatorWithoutController()
 }
