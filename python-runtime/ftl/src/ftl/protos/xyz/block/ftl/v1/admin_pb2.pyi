7edcfafaba4c6b170d964783a8442267ee999934
Moe Jangda <moe@tbd.email>
2024-11-04 22:16:13 -0600

    feat: codegen python classes from protos for schema extraction (#3301)
    # Summary
    This PR configures `buf` to generate python classes from protos to be
    used for schema generation
    
    > [!NOTE]
    > currently the codgen'd proto classes are configured to output into the
    `ftl` python sdk. it's likely that this will change if we separate
    python schema generation code from the python sdk itself. haven't
    decided yet, so defaulting to a single python package atm.
    
    
    > [!WARNING]
    > running `buf generate` changed the `connect-es` version in some ts
    proto classes from `v1.6.1` -> `v1.5.0`. not 100% sure why this
    happened. may be because protos haven't been generated in awhile? or
    maybe the `v1.6.1` came from when we were using remote plugins.
    @wesbillman @deniseli y'all might have more insight on this!
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ python-runtime/ftl/src/ftl/protos/xyz/block/ftl/v1/admin_pb2.pyi
@@ -0,0 +1,160 @@
+from xyz.block.ftl.v1 import ftl_pb2 as _ftl_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ConfigProvider(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    CONFIG_INLINE: _ClassVar[ConfigProvider]
+    CONFIG_ENVAR: _ClassVar[ConfigProvider]
+    CONFIG_DB: _ClassVar[ConfigProvider]
+
+class SecretProvider(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    SECRET_INLINE: _ClassVar[SecretProvider]
+    SECRET_ENVAR: _ClassVar[SecretProvider]
+    SECRET_KEYCHAIN: _ClassVar[SecretProvider]
+    SECRET_OP: _ClassVar[SecretProvider]
+    SECRET_ASM: _ClassVar[SecretProvider]
+CONFIG_INLINE: ConfigProvider
+CONFIG_ENVAR: ConfigProvider
+CONFIG_DB: ConfigProvider
+SECRET_INLINE: SecretProvider
+SECRET_ENVAR: SecretProvider
+SECRET_KEYCHAIN: SecretProvider
+SECRET_OP: SecretProvider
+SECRET_ASM: SecretProvider
+
+class ConfigRef(_message.Message):
+    __slots__ = ("module", "name")
+    MODULE_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    module: str
+    name: str
+    def __init__(self, module: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
+
+class ListConfigRequest(_message.Message):
+    __slots__ = ("module", "include_values", "provider")
+    MODULE_FIELD_NUMBER: _ClassVar[int]
+    INCLUDE_VALUES_FIELD_NUMBER: _ClassVar[int]
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    module: str
+    include_values: bool
+    provider: ConfigProvider
+    def __init__(self, module: _Optional[str] = ..., include_values: bool = ..., provider: _Optional[_Union[ConfigProvider, str]] = ...) -> None: ...
+
+class ListConfigResponse(_message.Message):
+    __slots__ = ("configs",)
+    class Config(_message.Message):
+        __slots__ = ("refPath", "value")
+        REFPATH_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        refPath: str
+        value: bytes
+        def __init__(self, refPath: _Optional[str] = ..., value: _Optional[bytes] = ...) -> None: ...
+    CONFIGS_FIELD_NUMBER: _ClassVar[int]
+    configs: _containers.RepeatedCompositeFieldContainer[ListConfigResponse.Config]
+    def __init__(self, configs: _Optional[_Iterable[_Union[ListConfigResponse.Config, _Mapping]]] = ...) -> None: ...
+
+class GetConfigRequest(_message.Message):
+    __slots__ = ("ref",)
+    REF_FIELD_NUMBER: _ClassVar[int]
+    ref: ConfigRef
+    def __init__(self, ref: _Optional[_Union[ConfigRef, _Mapping]] = ...) -> None: ...
+
+class GetConfigResponse(_message.Message):
+    __slots__ = ("value",)
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    value: bytes
+    def __init__(self, value: _Optional[bytes] = ...) -> None: ...
+
+class SetConfigRequest(_message.Message):
+    __slots__ = ("provider", "ref", "value")
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    REF_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    provider: ConfigProvider
+    ref: ConfigRef
+    value: bytes
+    def __init__(self, provider: _Optional[_Union[ConfigProvider, str]] = ..., ref: _Optional[_Union[ConfigRef, _Mapping]] = ..., value: _Optional[bytes] = ...) -> None: ...
+
+class SetConfigResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class UnsetConfigRequest(_message.Message):
+    __slots__ = ("provider", "ref")
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    REF_FIELD_NUMBER: _ClassVar[int]
+    provider: ConfigProvider
+    ref: ConfigRef
+    def __init__(self, provider: _Optional[_Union[ConfigProvider, str]] = ..., ref: _Optional[_Union[ConfigRef, _Mapping]] = ...) -> None: ...
+
+class UnsetConfigResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class ListSecretsRequest(_message.Message):
+    __slots__ = ("module", "include_values", "provider")
+    MODULE_FIELD_NUMBER: _ClassVar[int]
+    INCLUDE_VALUES_FIELD_NUMBER: _ClassVar[int]
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    module: str
+    include_values: bool
+    provider: SecretProvider
+    def __init__(self, module: _Optional[str] = ..., include_values: bool = ..., provider: _Optional[_Union[SecretProvider, str]] = ...) -> None: ...
+
+class ListSecretsResponse(_message.Message):
+    __slots__ = ("secrets",)
+    class Secret(_message.Message):
+        __slots__ = ("refPath", "value")
+        REFPATH_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        refPath: str
+        value: bytes
+        def __init__(self, refPath: _Optional[str] = ..., value: _Optional[bytes] = ...) -> None: ...
+    SECRETS_FIELD_NUMBER: _ClassVar[int]
+    secrets: _containers.RepeatedCompositeFieldContainer[ListSecretsResponse.Secret]
+    def __init__(self, secrets: _Optional[_Iterable[_Union[ListSecretsResponse.Secret, _Mapping]]] = ...) -> None: ...
+
+class GetSecretRequest(_message.Message):
+    __slots__ = ("ref",)
+    REF_FIELD_NUMBER: _ClassVar[int]
+    ref: ConfigRef
+    def __init__(self, ref: _Optional[_Union[ConfigRef, _Mapping]] = ...) -> None: ...
+
+class GetSecretResponse(_message.Message):
+    __slots__ = ("value",)
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    value: bytes
+    def __init__(self, value: _Optional[bytes] = ...) -> None: ...
+
+class SetSecretRequest(_message.Message):
+    __slots__ = ("provider", "ref", "value")
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    REF_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    provider: SecretProvider
+    ref: ConfigRef
+    value: bytes
+    def __init__(self, provider: _Optional[_Union[SecretProvider, str]] = ..., ref: _Optional[_Union[ConfigRef, _Mapping]] = ..., value: _Optional[bytes] = ...) -> None: ...
+
+class SetSecretResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class UnsetSecretRequest(_message.Message):
+    __slots__ = ("provider", "ref")
+    PROVIDER_FIELD_NUMBER: _ClassVar[int]
+    REF_FIELD_NUMBER: _ClassVar[int]
+    provider: SecretProvider
+    ref: ConfigRef
+    def __init__(self, provider: _Optional[_Union[SecretProvider, str]] = ..., ref: _Optional[_Union[ConfigRef, _Mapping]] = ...) -> None: ...
+
+class UnsetSecretResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
