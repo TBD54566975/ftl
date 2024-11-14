876aab0888b02b3bae8f437ad862fa874fecc2cd
Wes <wesbillman@users.noreply.github.com>
2024-11-14 15:03:34 -0700

    chore: opt-in to new react router features to silence warnings (#3390)
    ![Screenshot 2024-11-14 at 1 53
    01 PM](https://github.com/user-attachments/assets/23893cde-7730-4fac-bf91-a6eb131dcad6)

--- frontend/console/src/providers/routing-provider.tsx
+++ frontend/console/src/providers/routing-provider.tsx
@@ -28,8 +28,17 @@ const router = createBrowserRouter(
       <Route path='*' element={<NotFoundPage />} />
     </>,
   ),
+  {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  },
 )
 
 export const RoutingProvider = () => {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} future={{ v7_startTransition: true }} />
 }
