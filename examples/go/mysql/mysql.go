81e1e036f7e1920894f13543b76719aa3f50519d
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-11-22 10:53:52 +1100

    chore: mysql example

---
+++ examples/go/mysql/mysql.go
@@ -0,0 +1,72 @@
+package mysql
+
+import (
+	"context"
+
+	"github.com/TBD54566975/ftl/go-runtime/ftl" // Import the FTL SDK.
+)
+
+type MyDbConfig struct {
+	ftl.DefaultMySQLDatabaseConfig
+}
+
+func (MyDbConfig) Name() string { return "testdb" }
+
+type InsertRequest struct {
+	Data string
+}
+
+type InsertResponse struct{}
+
+//ftl:verb export
+func Insert(ctx context.Context, req InsertRequest, db ftl.DatabaseHandle[MyDbConfig]) (InsertResponse, error) {
+	err := persistRequest(ctx, req, db)
+	if err != nil {
+		return InsertResponse{}, err
+	}
+
+	return InsertResponse{}, nil
+}
+
+//ftl:verb export
+func Query(ctx context.Context, db ftl.DatabaseHandle[MyDbConfig]) ([]string, error) {
+	rows, err := db.Get(ctx).QueryContext(ctx, "SELECT data FROM requests")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []string
+	for rows.Next() {
+		var i string
+		if err := rows.Scan(
+			&i,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func persistRequest(ctx context.Context, req InsertRequest, db ftl.DatabaseHandle[MyDbConfig]) error {
+	_, err := db.Get(ctx).Exec(`CREATE TABLE IF NOT EXISTS requests
+(
+  data TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);`)
+	if err != nil {
+		return err
+	}
+	_, err = db.Get(ctx).Exec("INSERT INTO requests (data) VALUES (?);", req.Data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
