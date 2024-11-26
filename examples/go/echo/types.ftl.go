3c4ca76c208c2064790a6e849cadbad6bc139aed
Wes <wesbillman@users.noreply.github.com>
2024-11-26 12:24:38 -0700

    chore: commit types file for echo (#3536)

--- examples/go/echo/types.ftl.go
+++ examples/go/echo/types.ftl.go
@@ -2,10 +2,10 @@
 package echo
 
 import (
-    "context"
-    "github.com/TBD54566975/ftl/go-runtime/ftl/reflection"
-    "github.com/TBD54566975/ftl/go-runtime/server"
-    ftltime "ftl/time"
+	"context"
+	ftltime "ftl/time"
+	"github.com/TBD54566975/ftl/go-runtime/ftl/reflection"
+	"github.com/TBD54566975/ftl/go-runtime/server"
 )
 
 type EchoClient func(context.Context, EchoRequest) (EchoResponse, error)
@@ -13,8 +13,8 @@ type EchoClient func(context.Context, EchoRequest) (EchoResponse, error)
 func init() {
 	reflection.Register(
 		reflection.ProvideResourcesForVerb(
-            Echo,
-            server.VerbClient[ftltime.TimeClient, ftltime.TimeRequest, ftltime.TimeResponse](),
+			Echo,
+			server.VerbClient[ftltime.TimeClient, ftltime.TimeRequest, ftltime.TimeResponse](),
 			server.Config[string]("echo", "default"),
 		),
 	)
