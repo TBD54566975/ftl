33114020db1938ee79b98600c129cec7f5a9839c
Matt Toohey <mtoohey@block.xyz>
2024-12-04 08:22:59 +1100

    remove example code

--- examples/java/ExampleInterface.java
+++
@@ -1,8 +0,0 @@
-
-public interface ExampleInterface {
-    void instanceMethod();
-
-    static void staticMethod() {
-        System.out.println("This is a static method in an interface.");
-    }
-}
