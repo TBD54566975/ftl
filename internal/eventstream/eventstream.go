ca5e06fa9f95ffab63dcd602861d3a3559121812
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-12-06 17:29:21 +1100

    tmp

--- internal/eventstream/eventstream.go
+++ internal/eventstream/eventstream.go
@@ -4,77 +4,67 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/alecthomas/types/optional"
 	"github.com/alecthomas/types/pubsub"
 )
 
 // EventStream is a stream of events that can be published and subscribed to, that update a materialized view
-type EventStream[E any, T View[E]] interface {
-	Publish(Event[E, T]) error
+type EventStream[T View] interface {
+	Publish(Event[T]) error
 
 	View() T
 
-	Subscribe() <-chan Event[E, T]
+	Subscribe() <-chan Event[T]
 }
 
 // StreamView is a view of an event stream that can be subscribed to, without modifying the stream.
-type StreamView[E any, T View[E]] interface {
+type StreamView[T View] interface {
 	View() T
 
-	Subscribe() <-chan Event[E, T]
+	Subscribe() <-chan Event[T]
 }
 
 // View is a read-only view of the materialised current state of the event stream.
-type View[E any] interface {
-	// Entry returns the entry and the provided address, if it is present in the view
-	Entry(string) optional.Option[E]
-
-	// Entries returns all entries in the view
-	// these entries are not guaranteed to be in any particular order
-	Entries() []E
+type View interface {
 }
 
-type Event[E any, T View[E]] interface {
-	// Address returns the address of the event, which is used to determine where the event should be applied in the view
-	Address() string
+type Event[T View] interface {
 
-	// Handle applies the event to the view, by updating the existing entry if it is present
-	Handle(optional.Option[E], T) error
+	// Handle applies the event to the view
+	Handle(T) (T, error)
 }
 
-func NewInMemory[E any, T View[E]](initial T) EventStream[E, T] {
-	return &inMemoryEventStream[E, T]{
+func NewInMemory[T View](initial T) EventStream[T] {
+	return &inMemoryEventStream[T]{
 		view:  initial,
-		topic: pubsub.New[Event[E, T]](),
+		topic: pubsub.New[Event[T]](),
 	}
 
 }
 
-type inMemoryEventStream[E any, T View[E]] struct {
+type inMemoryEventStream[T View] struct {
 	view  T
 	lock  sync.Mutex
-	topic *pubsub.Topic[Event[E, T]]
+	topic *pubsub.Topic[Event[T]]
 }
 
-func (i *inMemoryEventStream[E, T]) Publish(e Event[E, T]) error {
+func (i *inMemoryEventStream[T]) Publish(e Event[T]) error {
 	i.lock.Lock()
 	defer i.lock.Unlock()
 
-	address := e.Address()
-	existing := i.view.Entry(address)
-	err := e.Handle(existing, i.view)
+	newView, err := e.Handle(i.view)
 	if err != nil {
 		return fmt.Errorf("failed to handle event: %w", err)
 	}
+	i.view = newView
 	i.topic.Publish(e)
 	return nil
 }
 
-func (i *inMemoryEventStream[E, T]) View() T {
+func (i *inMemoryEventStream[T]) View() T {
 	return i.view
 }
 
-func (i *inMemoryEventStream[E, T]) Subscribe() <-chan Event[E, T] {
+func (i *inMemoryEventStream[T]) Subscribe() <-chan Event[T] {
 	ret := i.topic.Subscribe(nil)
 	return ret
 }
