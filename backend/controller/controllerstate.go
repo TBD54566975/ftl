f6187ef97b9f751ec6276dd7c54a98f57c67d288
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-12-06 17:29:21 +1100

    tmp

--- backend/controller/controllerstate.go
+++ backend/controller/controllerstate.go
@@ -1,32 +1,43 @@
 package controller
 
 import (
+	"fmt"
 	"net/url"
 	"time"
 
 	"github.com/alecthomas/types/optional"
-	"golang.org/x/exp/maps"
 
 	"github.com/TBD54566975/ftl/internal/eventstream"
 	"github.com/TBD54566975/ftl/internal/model"
 )
 
-var _ eventstream.View[Runner] = (*RunnerState)(nil)
-
-type RunnerState struct {
-	runners map[string]Runner
+type State struct {
+	runners             map[string]*Runner
+	runnersByDeployment map[string][]*Runner
 }
 
-func (r *RunnerState) Entry(s string) optional.Option[Runner] {
+func (r *State) Runner(s string) optional.Option[Runner] {
 	result, ok := r.runners[s]
 	if ok {
-		return optional.Some(result)
+		return optional.Ptr(result)
 	}
 	return optional.None[Runner]()
 }
 
-func (r *RunnerState) Entries() []Runner {
-	return maps.Values(r.runners)
+func (r *State) Runners() []Runner {
+	var ret []Runner
+	for _, v := range r.runners {
+		ret = append(ret, *v)
+	}
+	return ret
+}
+
+func (r *State) RunnersForDeployment(deployment string) []Runner {
+	var ret []Runner
+	for _, v := range r.runnersByDeployment[deployment] {
+		ret = append(ret, *v)
+	}
+	return ret
 }
 
 type Runner struct {
@@ -36,4 +47,56 @@ type Runner struct {
 	Endpoint   *url.URL
 	Module     string
 	Deployment model.DeploymentKey
+}
+
+var _ eventstream.Event[State] = (*RunnerCreatedEvent)(nil)
+var _ eventstream.Event[State] = (*RunnerHeartbeatEvent)(nil)
+var _ eventstream.Event[State] = (*RunnerDeletedEvent)(nil)
+
+type RunnerCreatedEvent struct {
+	Key        string
+	Create     time.Time
+	Endpoint   *url.URL
+	Module     string
+	Deployment model.DeploymentKey
+}
+
+type RunnerHeartbeatEvent struct {
+	Key      string
+	LastSeen time.Time
+}
+
+func (r *RunnerHeartbeatEvent) Handle(t State) (State, error) {
+	existing := t.runners[r.Key]
+	if existing == nil {
+		return t, fmt.Errorf("runner %s not found", r.Key)
+	}
+	existing.LastSeen = r.LastSeen
+	return t, nil
+}
+
+type RunnerDeletedEvent struct {
+	Key string
+}
+
+func (r RunnerDeletedEvent) Handle(t State) (State, error) {
+	existing := t.runners[r.Key]
+	if existing != nil {
+		delete(t.runners, r.Key)
+
+	}
+	return t, nil
+}
+
+func (r *RunnerCreatedEvent) Handle(t State) (State, error) {
+	n := Runner{
+		Key:        r.Key,
+		Create:     r.Create,
+		Endpoint:   r.Endpoint,
+		Module:     r.Module,
+		Deployment: r.Deployment,
+	}
+	t.runners[r.Key] = &n
+	t.runnersByDeployment[r.Deployment.String()] = append(t.runnersByDeployment[r.Deployment.String()], &n)
+	return t, nil
 }
