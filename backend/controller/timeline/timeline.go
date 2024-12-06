c5f8097255bfc4da0bf6573618fc470e98bfb9b5
Matt Toohey <mtoohey@block.xyz>
2024-12-06 12:12:57 +1100

    fix: some events weren’t being published to new timeline service (#3650)

--- backend/controller/timeline/timeline.go
+++ backend/controller/timeline/timeline.go
@@ -3,13 +3,9 @@ package timeline
 import (
 	"context"
 	stdsql "database/sql"
-	"time"
-
-	"github.com/alecthomas/atomic"
 
 	"github.com/TBD54566975/ftl/backend/controller/encryption"
 	"github.com/TBD54566975/ftl/backend/controller/timeline/internal/sql"
-	"github.com/TBD54566975/ftl/internal/log"
 )
 
 type EventType = sql.EventType
@@ -41,39 +37,16 @@ type InEvent interface {
 }
 
 type Service struct {
-	ctx              context.Context
-	conn             *stdsql.DB
-	encryption       *encryption.Service
-	events           chan Event
-	lastDroppedError atomic.Value[time.Time]
-	lastFailedError  atomic.Value[time.Time]
+	ctx        context.Context
+	conn       *stdsql.DB
+	encryption *encryption.Service
 }
 
 func New(ctx context.Context, conn *stdsql.DB, encryption *encryption.Service) *Service {
-	var s *Service
-	events := make(chan Event, 1000)
-	s = &Service{
+	s := &Service{
 		ctx:        ctx,
 		conn:       conn,
 		encryption: encryption,
-		events:     events,
 	}
 	return s
-}
-
-// EnqueueEvent asynchronously enqueues an event for insertion into the timeline.
-func (s *Service) EnqueueEvent(ctx context.Context, inEvent InEvent) {
-	event, err := inEvent.toEvent()
-	if err != nil {
-		log.FromContext(ctx).Warnf("Failed to convert event to event: %v", err)
-		return
-	}
-	select {
-	case s.events <- event:
-	default:
-		if time.Since(s.lastDroppedError.Load()) > 10*time.Second {
-			log.FromContext(ctx).Warnf("Dropping event %T due to full queue", event)
-			s.lastDroppedError.Store(time.Now())
-		}
-	}
 }
