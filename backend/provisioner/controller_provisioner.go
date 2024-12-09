dcbb10a4136892905fcab4dac83522fcac85b47c
Juho Makinen <juho@squareup.com>
2024-12-09 16:30:07 +1100

    chore: refactoring

--- backend/provisioner/controller_provisioner.go
+++ backend/provisioner/controller_provisioner.go
@@ -15,11 +15,16 @@ import (
 // NewControllerProvisioner creates a new provisioner that uses the FTL controller to provision modules
 func NewControllerProvisioner(client ftlv1connect.ControllerServiceClient) *InMemProvisioner {
 	return NewEmbeddedProvisioner(map[schema.ResourceType]InMemResourceProvisionerFn{
-		schema.ResourceTypeModule: func(ctx context.Context, res schema.Provisioned, module *schema.Module) (*RuntimeEvent, error) {
+		schema.ResourceTypeModule: func(ctx context.Context, moduleName string, res schema.Provisioned) (*RuntimeEvent, error) {
 			logger := log.FromContext(ctx)
-			logger.Debugf("Provisioning module: %s", module)
+			logger.Debugf("Provisioning module: %s", moduleName)
 
 			var artefacts []*ftlv1.DeploymentArtefact
+
+			module, ok := res.(*schema.Module)
+			if !ok {
+				return nil, fmt.Errorf("expected module, got %T", res)
+			}
 
 			for _, artefact := range module.Metadata {
 				if metadata, ok := artefact.(*schema.MetadataArtefact); ok {
