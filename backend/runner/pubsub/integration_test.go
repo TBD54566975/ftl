1205f98444a734abb223ff3d1d3f0a54d8925a81
Matt Toohey <mtoohey@block.xyz>
2024-12-12 10:47:01 +1100

    skipped test no longer builds

--- backend/runner/pubsub/integration_test.go
+++ backend/runner/pubsub/integration_test.go
@@ -3,16 +3,13 @@
 package pubsub
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"connectrpc.com/connect"
-	"github.com/TBD54566975/ftl/backend/controller/async"
 	timelinepb "github.com/TBD54566975/ftl/backend/protos/xyz/block/ftl/timeline/v1"
 	in "github.com/TBD54566975/ftl/internal/integration"
 	"github.com/TBD54566975/ftl/internal/model"
-	"github.com/TBD54566975/ftl/internal/schema"
 	"github.com/TBD54566975/ftl/internal/slices"
 	"github.com/alecthomas/assert/v2"
 )
@@ -88,75 +85,77 @@ func checkSuccessfullyConsumed(module, verb string, count int) in.Action {
 
 func TestRetry(t *testing.T) {
 	t.Skip("Not implemented for new pubsub yet")
-	retriesPerCall := 2
-	in.Run(t,
-		in.WithLanguages("java", "go"),
-		in.CopyModule("publisher"),
-		in.CopyModule("subscriber"),
-		in.Deploy("publisher"),
-		in.Deploy("subscriber"),
+	// 	retriesPerCall := 2
+	// 	in.Run(t,
+	// 		in.WithLanguages("java", "go"),
+	// 		in.CopyModule("publisher"),
+	// 		in.CopyModule("subscriber"),
+	// 		in.Deploy("publisher"),
+	// 		in.Deploy("subscriber"),
 
-		// publish events
-		in.Call("publisher", "publishOneToTopic2", in.Obj{}, func(t testing.TB, resp in.Obj) {}),
+	// 		// publish events
+	// 		in.Call("publisher", "publishOneToTopic2", in.Obj{}, func(t testing.TB, resp in.Obj) {}),
 
-		in.Sleep(time.Second*6),
+	// 		in.Sleep(time.Second*6),
 
-		// check that there are the right amount of failed async calls to the verb
-		in.QueryRow("ftl",
-			fmt.Sprintf(`
-				SELECT COUNT(*)
-				FROM async_calls
-				WHERE
-					state = 'error'
-					AND verb = 'subscriber.consumeButFailAndRetry'
-					AND catching = false
-					AND origin = '%s'
-		`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
-			1+retriesPerCall),
+	// 		// check that there are the right amount of failed async calls to the verb
+	// 		in.QueryRow("ftl",
+	// 			fmt.Sprintf(`
+	// 				SELECT COUNT(*)
+	// 				FROM async_calls
+	// 				WHERE
+	// 					state = 'error'
+	// 					AND verb = 'subscriber.consumeButFailAndRetry'
+	// 					AND catching = false
+	// 					AND origin = '%s'
+	// 		`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
+	// 			1+retriesPerCall),
 
-		// check that there is one failed attempt to catch (we purposely fail the first one)
-		in.QueryRow("ftl",
-			fmt.Sprintf(`
-			SELECT COUNT(*)
-			FROM async_calls
-			WHERE
-				state = 'error'
-				AND verb = 'subscriber.consumeButFailAndRetry'
-				AND error LIKE '%%subscriber.catch %%'
-				AND error LIKE '%%catching error%%'
-				AND catching = true
-				AND origin = '%s'
-	`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
-			1),
+	// 		// check that there is one failed attempt to catch (we purposely fail the first one)
+	// 		in.QueryRow("ftl",
+	// 			fmt.Sprintf(`
+	// 			SELECT COUNT(*)
+	// 			FROM async_calls
+	// 			WHERE
+	// 				state = 'error'
+	// 				AND verb = 'subscriber.consumeButFailAndRetry'
+	// 				AND error LIKE '%%subscriber.catch %%'
+	// 				AND error LIKE '%%catching error%%'
+	// 				AND catching = true
+	// 				AND origin = '%s'
+	// 	`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
+	// 			1),
 
-		// check that there is one successful attempt to catch (we succeed the second one as long as we receive the correct error in the request)
-		in.QueryRow("ftl",
-			fmt.Sprintf(`
-		SELECT COUNT(*)
-		FROM async_calls
-		WHERE
-			state = 'success'
-			AND verb = 'subscriber.consumeButFailAndRetry'
-			AND error IS NULL
-			AND catching = true
-			AND origin = '%s'
-`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
-			1),
+	// 		// check that there is one successful attempt to catch (we succeed the second one as long as we receive the correct error in the request)
+	// 		in.QueryRow("ftl",
+	// 			fmt.Sprintf(`
+	// 		SELECT COUNT(*)
+	// 		FROM async_calls
+	// 		WHERE
+	// 			state = 'success'
+	// 			AND verb = 'subscriber.consumeButFailAndRetry'
+	// 			AND error IS NULL
+	// 			AND catching = true
+	// 			AND origin = '%s'
+	// `, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndRetry"}}.String()),
+	// 			1),
 
-		// check that there was one successful attempt to catchAny
-		in.QueryRow("ftl",
-			fmt.Sprintf(`
-		SELECT COUNT(*)
-		FROM async_calls
-		WHERE
-			state = 'success'
-			AND verb = 'subscriber.consumeButFailAndCatchAny'
-			AND error IS NULL
-			AND catching = true
-			AND origin = '%s'
-`, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndCatchAny"}}.String()),
-			1),
-	)
+	//	// check that there was one successful attempt to catchAny
+	//	in.QueryRow("ftl",
+	//		fmt.Sprintf(`
+	//	SELECT COUNT(*)
+	//	FROM async_calls
+	//	WHERE
+	//		state = 'success'
+	//		AND verb = 'subscriber.consumeButFailAndCatchAny'
+	//		AND error IS NULL
+	//		AND catching = true
+	//		AND origin = '%s'
+	//
+	// `, async.AsyncOriginPubSub{Subscription: schema.RefKey{Module: "subscriber", Name: "consumeButFailAndCatchAny"}}.String()),
+	//
+	//			1),
+	//	)
 }
 
 func TestExternalPublishRuntimeCheck(t *testing.T) {
